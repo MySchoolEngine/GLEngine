7bf2f6282beb7daa86366ccb311e26bac96c58ae
Dominik Roháček <RohacekD@gmail.com>
2019-03-02 02:43:42 +0100

    Logging only filename instead of full path

--- Core/src/Core/Logging/LoggingMacros.h
+++ Core/src/Core/Logging/LoggingMacros.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <Core/Utils/CompileTimeUtils.h>
+
 #ifdef GL_ENGINE_DEBUG
-	#define CORE_LOG(level, ctx, message, ...) Core::Logging::C_LoggingSystem::Instance().Log(level, ctx, __LINE__, __FILE__, message)
+	#define CORE_LOG(level, ctx, message, ...) Core::Logging::C_LoggingSystem::Instance().Log(level, ctx, __LINE__, __FILENAME__, message)
 #else
 	#define CORE_LOG(level, ctx, message, ...)
 #endif
