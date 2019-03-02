62ccc13818fbc901c3b9a230e33638ab77c5e2a4
Dominik Roháček <RohacekD@gmail.com>
2019-03-02 05:06:02 +0100

    More examples added into Sandbox

--- Sandbox/src/Sandbox/SandboxApp.cpp
+++ Sandbox/src/Sandbox/SandboxApp.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
-#include <Core/Example.h>
+#include <Core/CoreMacros.h>
+
+#include <Core/GLFW/GLFWWindow.h>
+#include <Core/GLFW/GLFWWindowManager.h>
+#include <Core/Logging/Logging.h>
+#include <Core/Logging/ILogger.h>
 
 int main(int args, char** argv) {
-	std::cout << "asd\n";
-	Example ex;
+	auto& logging = Core::Logging::C_LoggingSystem::Instance();
+	logging.AddLogger(new Core::Logging::C_CoutLogger());
+	//logging.AddLogger(new Core::Logging::C_FileLogger("log.txt"));
+
+	Core::S_WindowInfo info;
+	info.m_name = "My title";
+
+	Core::I_WindowManager* wmng = new Core::C_GLFWWindowManager();
+	wmng->OpenNewWindow(info);
+
+	while (true) {
+		wmng->Update();
+	}
+
 	return 0;
 }
