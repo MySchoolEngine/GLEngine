b86c930025e5a716bfe4dfdb126dedd2e97ebcdf
Dominik Roháček <RohacekD@gmail.com>
2019-03-02 02:57:49 +0100

    Added file loggger

--- Core/src/Core/Logging/ILogger.cpp
+++ Core/src/Core/Logging/ILogger.cpp
@@ -14,5 +14,25 @@ void C_CoutLogger::Log(E_Level level, E_Context context, int line, const char* f
 	std::cout << file << ":" << line << "(" << level << "," << context << "): " << text;
 }
 
+//=================================================================================
+C_FileLogger::C_FileLogger(const std::string& filename)
+	: m_file(new std::remove_pointer<decltype(m_file)>::type)
+{
+	m_file->open(filename);
+}
+
+//=================================================================================
+C_FileLogger::~C_FileLogger()
+{
+	m_file->close();
+	delete m_file;
+}
+
+//=================================================================================
+void C_FileLogger::Log(E_Level level, E_Context context, int line, const char* file, const std::string& text)
+{
+	(*m_file) << file << ":" << line << "(" << level << "," << context << "): " << text;
+}
+
 }
 }
--- Core/src/Core/Logging/ILogger.h
+++ Core/src/Core/Logging/ILogger.h
@@ -5,6 +5,7 @@
 #include <Core/Logging/LoggingTypes.h>
 
 #include <string>
+#include <fstream>
 
 namespace Core {
 namespace Logging {
@@ -18,6 +19,16 @@ public:
 class API_EXPORT C_CoutLogger : public I_Logger {
 public:
 	virtual void Log(E_Level level, E_Context context, int line, const char* file, const std::string& text) override;
+};
+
+class API_EXPORT C_FileLogger : public I_Logger {
+public:
+	C_FileLogger(const std::string& filename);
+	virtual ~C_FileLogger();
+	virtual void Log(E_Level level, E_Context context, int line, const char* file, const std::string& text) override;
+
+protected:
+	std::ofstream* m_file;
 };
 }
 }
