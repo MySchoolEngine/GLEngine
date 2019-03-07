a3b092088bc744ac4b68653279d3f831c2743c1c
Dominik Roháček <RohacekD@gmail.com>
2019-03-07 12:00:11 +0100

    Completly basic interfaces for Batch and command. I really need to move on

---
+++ Renderer/src/Renderer/IRenderBatch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace GLEngine {
+namespace Renderer {
+
+//================================================
+// List of I_RenderCommands
+// should be in this order:
+// 1] state changing commands
+// 2] draw calls
+// 3] possible getters
+// would be nice to be able to define dependencies between batches to e.g. push all
+// shadow map draw calls at beginning, than calculate smth else and draw calls which need to 
+// read from shadow map at the end
+//================================================
+class I_RenderBatch {
+public:
+	virtual ~I_RenderBatch() = default;
+};
+
+}
+}
