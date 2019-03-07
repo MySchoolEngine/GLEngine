239480cdfa8c43d96d07c7fb2c923bd4ac9897b1
Dominik Roháček <RohacekD@gmail.com>
2019-03-07 11:33:47 +0100

    Basic extractor interface added

---
+++ Renderer/src/Renderer/IExtractor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <memory>
+
+namespace GLEngine {
+namespace Renderer {
+
+class I_RenderableComponent;
+class I_RawGPUData;
+
+template<class Extractor>
+class I_Extractor {
+public:
+	static std::unique_ptr<I_RawGPUData> Extract(std::shared_ptr<I_RenderableComponent> component) {
+		return Extractor::ExtractInner(component);
+	}
+};
+
+// leave it here for documentation now
+/* class C_Extractor : public I_Extractor<C_Extractor> {
+public:
+	static std::unique_ptr<I_RawGPUData> ExtractInner(std::shared_ptr<I_RenderableComponent> component) {
+		return nullptr;
+	}
+};*/
+
+}
+}
+
