cf6bfd3b091a836fa6b216d40da9a4dbfd9873ca
Dominik Roháček <RohacekD@gmail.com>
2019-03-07 12:49:19 +0100

    Added Renderer interface

---
+++ Renderer/src/Renderer/IRenderer.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <memory>
+
+namespace GLEngine {
+namespace Renderer {
+
+class I_RenderCommand;
+class I_RenderBatch;
+
+// We need some separated tasks -> game engine should give us list of all visible objects
+// or some way to ask him what is inside given frustum
+// than we need to be able to collect data to internal structures
+// than convert them to GPU-friendly data
+// than collect draw calls, render batches etc.
+// sort them to some convenient order, and commit them to GPU
+// in this way we can split one frame to separated parts
+// 1] simulation
+// 2] data collection (simulation have to be stopped in this moment, so everything we would be able to do here is just collection in tight loop)
+// 3] data conversion - now we can begin with simulation of new frame
+// 4] commit to GPU
+// 5] check the time, if we have enough time, render thread can help with simulation, game computation etc.
+// 6] render new frame
+class I_Renderer {
+public:
+	virtual ~I_Renderer() = default;
+	virtual void AddCommand(std::unique_ptr<I_RenderCommand>) = 0;
+	virtual void AddBatch(std::unique_ptr<I_RenderBatch>) = 0;
+
+	/** ==============================================
+	 * @method:    SortCommands
+	 * @return:    void
+	 * @brief	   Reorganize all commands for better performance
+	 ** ==============================================*/
+	virtual void SortCommands() = 0;
+
+	/** ==============================================
+	 * @method:    ExtractData
+	 * @return:    void
+	 * @brief	   By now we should have all commands to render the frame
+	 *			   and we start with extraction of I_RawGPUData
+	 ** ==============================================*/
+	virtual void ExtractData() = 0;
+
+	/** ==============================================
+	 * @method:    TransformData
+	 * @return:    void
+	 * @brief	   After we collected all data, we have time to transform them
+	 *			   in this time the simulation thread is free to change 
+	 *			   it's renderable data
+	 ** ==============================================*/
+	virtual void TransformData() = 0;
+
+	/** ==============================================
+	* @method:    Commit
+	* @return:    void
+	* @brief	   Commits all GPU calls to GPU
+	** ==============================================*/
+	virtual void Commit() const = 0;
+
+	/** ==============================================
+	* @method:    ClearCommandBuffers
+	* @return:    void
+	* @brief	   Should be called at the end of frame.
+	** ==============================================*/
+	virtual void ClearCommandBuffers() = 0;
+};
+
+}
+}
