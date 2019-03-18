8fd07737b1b814fde9fd0afcc311f0ed56154205
Dominik Roháček <RohacekD@gmail.com>
2019-03-18 14:01:48 +0100

    GLClearColor implementation

---
+++ GLRenderer/GLRenderer/Commands/GlClearColor.cpp
@@ -0,0 +1,40 @@
+#include <GLRenderer/Commands/GlClearColor.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace GLEngine {
+namespace GLRenderer {
+namespace Commands {
+
+//=================================================================================
+C_GLClearColor::C_GLClearColor(glm::vec4&& color)
+	: m_Colour(std::move(color))
+{}
+
+//=================================================================================
+C_GLClearColor::C_GLClearColor(glm::vec3 color)
+	: C_GLClearColor(glm::vec4(color, 0.0f))
+{}
+
+//=================================================================================
+void C_GLClearColor::Commit()
+{
+	glClearColor(m_Colour.r, m_Colour.g, m_Colour.b, m_Colour.a);
+}
+
+//=================================================================================
+GLEngine::Renderer::I_RenderCommand::E_Type C_GLClearColor::GetType() const
+{
+	return Renderer::I_RenderCommand::E_Type::StateChange;
+}
+
+//=================================================================================
+std::shared_ptr<GLEngine::Renderer::I_Resource> C_GLClearColor::GetResource() const
+{
+	return nullptr;
+}
+
+}
+}
+}
---
+++ GLRenderer/GLRenderer/Commands/GlClearColor.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <Core/CoreMacros.h>
+
+#include <Renderer/IRenderCommand.h>
+
+#include <glm/vec4.hpp>
+#include <glm/vec3.hpp>
+
+namespace GLEngine {
+namespace GLRenderer {
+namespace Commands {
+
+class C_GLClearColor : public Renderer::I_RenderCommand {
+public:
+	C_GLClearColor(glm::vec4&& color);
+	C_GLClearColor(glm::vec3 color);
+
+	//===========================================
+	// Renderer::I_RenderCommand
+	//===========================================
+	virtual void Commit() override;
+	virtual E_Type GetType() const override;
+	virtual std::shared_ptr<Renderer::I_Resource> GetResource() const override;
+
+
+protected:
+	glm::vec4 m_Colour;
+};
+
+}}}
