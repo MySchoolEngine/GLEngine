3b012ee1245553d8740b422898ec02c49eef8dc1
Dominik Roháček <RohacekD@gmail.com>
2019-03-20 17:53:20 +0100

    Added glm into stdafx

--- GLRenderer/GLRendererStdafx.h
+++ GLRenderer/GLRendererStdafx.h
@@ -9,8 +9,11 @@
 #include <algorithm>
 #include <fstream>
 #include <regex>
+#include <stack>
 #include <exception>
 
 // rendering dependend things
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
