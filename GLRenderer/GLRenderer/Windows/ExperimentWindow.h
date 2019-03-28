465541238d7c522967705ad5c5d7a7105bda1a69
Dominik Roháček <RohacekD@gmail.com>
2019-03-28 13:38:56 +0100

    Example hacked window for merge

--- GLRenderer/GLRenderer/Windows/ExperimentWindow.h
+++ GLRenderer/GLRenderer/Windows/ExperimentWindow.h
@@ -4,12 +4,31 @@
 
 namespace GLEngine {
 namespace GLRenderer {
+
+namespace Cameras {
+class C_OrbitalCamera;
+}
+
+namespace Mesh {
+class C_StaticMeshResource;
+}
+
+namespace Buffers {
+namespace UBO {
+class C_FrameConstantsBuffer;
+}
+}
+
 namespace Windows {
 class C_ExplerimentWindow : public GLFW::C_GLFWoGLWindow {
 public:
 	C_ExplerimentWindow(const Core::S_WindowInfo& wndInfo);
 	//=================================================================================
 	virtual void Update() override;
+private:
+	std::shared_ptr<Mesh::C_StaticMeshResource>					m_Mesh;
+	std::shared_ptr<Buffers::UBO::C_FrameConstantsBuffer>		m_FrameConstUBO;
+	std::shared_ptr<Cameras::C_OrbitalCamera>					m_OrbitalCamera;
 
 };
 }
