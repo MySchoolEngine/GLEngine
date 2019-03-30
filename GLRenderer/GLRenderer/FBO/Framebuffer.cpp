1a7e532938fafba7f667852537164b76712adae6
Dominik Roháček <RohacekD@gmail.com>
2019-03-30 01:21:32 +0100

    Resolved #36

--- GLRenderer/GLRenderer/FBO/Framebuffer.cpp
+++ GLRenderer/GLRenderer/FBO/Framebuffer.cpp
@@ -1,61 +1,51 @@
-#include "GLW/Framebuffer.h"
-#include "GLW/Texture.h"
+#include <GLRendererStdafx.h>
 
-#include "Debug/Debug.h"
+#include <GLRenderer/FBO/Framebuffer.h>
+#include <GLRenderer/Textures/Texture.h>
 
-#include <iostream>
+namespace GLEngine {
+namespace GLRenderer {
+namespace Components {
 
-namespace GLW {
 
-
-	//=================================================================================
-	C_Framebuffer::C_Framebuffer()
-	{
-		glGenFramebuffers(1, &m_FBO);
-
-	}
-
-	//=================================================================================
-	C_Framebuffer::~C_Framebuffer()
-	{
-		DestructorFullCheck();
-	}
-
-	//=================================================================================
-	void C_Framebuffer::Bind()
-	{
-		glBindFramebuffer(GL_FRAMEBUFFER, m_FBO);
-		ErrorCheck();
-	}
-
-	//=================================================================================
-	void C_Framebuffer::Unbind()
-	{
-		ErrorCheck();
-		// switch back to window-system-provided framebuffer
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		ErrorCheck();
-	}
-
-	//=================================================================================
-	void C_Framebuffer::AttachTexture(GLenum attachement, std::shared_ptr<C_Texture> texture)
-	{
-		if (m_attachements.find(attachement) != m_attachements.end()) {
-			std::cerr << "Attaching multiple textures to one attach point in framebuffer" << std::endl;
-		}
-		Bind();
-		glFramebufferTexture(GL_FRAMEBUFFER, attachement, texture->GetTexture(), 0);
-		Unbind();
-
-		ErrorCheck();
-		m_attachements[attachement] = texture;
-	}
-
-	//=================================================================================
-	std::shared_ptr<GLW::C_Texture> C_Framebuffer::GetAttachement(GLenum attachement)
-	{
-		assert(m_attachements.find(attachement) != m_attachements.end());
-		return m_attachements[attachement];
-	}
+//=================================================================================
+C_Framebuffer::C_Framebuffer()
+{
+	glGenFramebuffers(1, &m_FBO);
 
 }
+
+//=================================================================================
+void C_Framebuffer::Bind()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, m_FBO);
+}
+
+//=================================================================================
+void C_Framebuffer::Unbind()
+{
+	// switch back to window-system-provided framebuffer
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+//=================================================================================
+void C_Framebuffer::AttachTexture(GLenum attachement, std::shared_ptr<Textures::C_Texture> texture)
+{
+	if (m_attachements.find(attachement) != m_attachements.end()) {
+		std::cerr << "Attaching multiple textures to one attach point in framebuffer" << std::endl;
+	}
+	Bind();
+	glFramebufferTexture(GL_FRAMEBUFFER, attachement, texture->GetTexture(), 0);
+	Unbind();
+
+	m_attachements[attachement] = texture;
+}
+
+//=================================================================================
+std::shared_ptr<Textures::C_Texture> C_Framebuffer::GetAttachement(GLenum attachement)
+{
+	assert(m_attachements.find(attachement) != m_attachements.end());
+	return m_attachements[attachement];
+}
+
+}}}
--- GLRenderer/GLRenderer/FBO/Framebuffer.h
+++ GLRenderer/GLRenderer/FBO/Framebuffer.h
@@ -13,27 +13,28 @@
 
 #pragma once
 
-#include <QOpenGLFunctions_4_3_Core>
 
-#include <map>
-#include <memory>
+namespace GLEngine {
+namespace GLRenderer {
 
-
-namespace GLW {
-	class C_Texture;
-	class C_Framebuffer : public QOpenGLFunctions_4_3_Core {
-	public:
-		C_Framebuffer();
-		~C_Framebuffer();
-
-		void Bind();
-		void Unbind();
-
-		void AttachTexture(GLenum attachement, std::shared_ptr<C_Texture> texture);
-		std::shared_ptr<C_Texture> GetAttachement(GLenum attachement);
-
-	private:
-		GLuint m_FBO;
-		std::map<GLenum, std::shared_ptr<C_Texture>> m_attachements;
-	};
+namespace Textures {
+class C_Texture;
 }
+
+namespace Components {
+class C_Framebuffer {
+public:
+	C_Framebuffer();
+	~C_Framebuffer() = default;
+
+	void Bind();
+	void Unbind();
+
+	void AttachTexture(GLenum attachement, std::shared_ptr<Textures::C_Texture> texture);
+	std::shared_ptr<Textures::C_Texture> GetAttachement(GLenum attachement);
+
+private:
+	GLuint m_FBO;
+	std::map<GLenum, std::shared_ptr<C_Texture>> m_attachements;
+};
+}}}
