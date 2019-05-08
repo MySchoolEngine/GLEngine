7277a1984b214d027a81233567b5835b740276c5
Dominik Roháček <RohacekD@gmail.com>
2019-05-09 00:08:58 +0200

    First drop simulation. Droplet is going uphil, instead of donwhill

--- data/Shaders/terrain/erosion.glsl
+++ data/Shaders/terrain/erosion.glsl
@@ -15,12 +15,30 @@ layout (binding = 1) uniform rainData{
 //=================================================================================
 struct Droplet
 {
-  ivec2 position;
+  vec2 position;
   vec2 direction;
   float velocity;
   float water;
   float sediment;
 };
+
+vec3 GetGradientAndHeight(vec2 posf){
+	const vec2 size = vec2(0.3,0.0);
+	const ivec3 off = ivec3(-1,0,1);
+	const ivec2 pos = ivec2(posf.x, posf.y);
+	
+	vec4 wave = imageLoad(terrainPatch, pos);
+	float s11 = wave.x;
+    float s01 = imageLoad(terrainPatch, pos+off.xy).x;//[-1,0]
+    float s21 = imageLoad(terrainPatch, pos+ off.zy).x;//[1,0]
+    float s10 = imageLoad(terrainPatch, pos+ off.yx).x;//[0,-1]
+    float s12 = imageLoad(terrainPatch, pos+ off.yz).x;//[0,1]
+	float gradX = s21-s01;
+	float gradY = s12-s10;
+
+
+    return vec3(gradX, gradY, wave.x);
+}
 
 //=================================================================================
 void main()
@@ -32,12 +50,19 @@ void main()
 	}
 
     Droplet drop;
-    drop.position = rain.droplets[posX];
+    ivec2 posI = rain.droplets[posX];
+    drop.position = vec2(posI.x, posI.y);
     drop.direction = vec2(0.0, 0.0);
     drop.velocity = 1.0f;// starting speed
     drop.water = 1.0f; // starting water
     drop.sediment = 0.0f;// cause it doesnt took any sediment yet
 
-	float val = imageLoad(terrainPatch, drop.position).x - 50.0f/255.0f;
-	imageStore(terrainPatch, drop.position, vec4(0, val, val, 1));
+    for(int i = 0;i<10;++i){
+    	drop.direction = normalize(GetGradientAndHeight(drop.position).xy);
+		float val = imageLoad(terrainPatch, ivec2(drop.position)).x - 4.0f/255.0f;
+		imageStore(terrainPatch, ivec2(drop.position), vec4(val, val, val, 1));
+
+		drop.position += drop.direction;
+	}
+
 }  
