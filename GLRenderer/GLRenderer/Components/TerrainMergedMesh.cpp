58da66903fc13a677da9671bd8e0946e9679ad7f
Dominik Roháček <RohacekD@gmail.com>
2019-05-08 14:23:10 +0200

    Added terrain merged mesh component

---
+++ GLRenderer/GLRenderer/Components/TerrainMergedMesh.cpp
@@ -0,0 +1,25 @@
+#include <GLRendererStdafx.h>
+
+#include <GLRenderer/Components/TerrainMergedMesh.h>
+
+
+namespace GLEngine {
+namespace GLRenderer {
+namespace Components {
+
+//=================================================================================
+C_TerrainMergedMesh::C_TerrainMergedMesh(std::vector<std::shared_ptr<C_TerrainMesh>> meshes)
+	: m_Meshes(meshes)
+{
+
+}
+
+//=================================================================================
+void C_TerrainMergedMesh::PerformDraw() const
+{
+	std::for_each(m_Meshes.begin(), m_Meshes.end(), [](std::shared_ptr<C_TerrainMesh> mesh) {
+		mesh->PerformDraw();
+	});
+}
+
+}}}
---
+++ GLRenderer/GLRenderer/Components/TerrainMergedMesh.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Renderer/IRenderableComponent.h>
+
+#include <GLRenderer/Components/TerrainMesh.h>
+
+namespace GLEngine {
+namespace GLRenderer {
+namespace Components {
+class C_TerrainMergedMesh : public Renderer::I_RenderableComponent {
+public:
+	// @todo fix that, this class is just a hack
+	C_TerrainMergedMesh(std::vector<std::shared_ptr<C_TerrainMesh>> meshes);
+	~C_TerrainMergedMesh() = default;
+
+
+	//=======================================================
+	virtual void PerformDraw() const override;
+
+protected:
+	std::vector<std::shared_ptr<C_TerrainMesh>> m_Meshes;
+};
+}}}
