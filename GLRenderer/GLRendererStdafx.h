e132fb3df53ee1fdaad5a84e7ed4b64646fd2d5f
Dominik Roháček <RohacekD@gmail.com>
2019-05-13 01:20:14 +0200

    Numeric added to GLRendererStdafx.h and sorted std includes

--- GLRenderer/GLRendererStdafx.h
+++ GLRenderer/GLRendererStdafx.h
@@ -2,22 +2,22 @@
 
 #include <Utils/Logging/LoggingMacros.h>
 
-#include <iostream>
-#include <memory>
-#include <vector>
-#include <string>
-#include <sstream>
 #include <algorithm>
-#include <fstream>
-#include <regex>
-#include <stack>
+#include <array>
+#include <chrono>
 #include <exception>
 #include <functional>
-#include <map>
-#include <array>
-#include <random>
-#include <chrono>
+#include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <memory>
+#include <map>
+#include <numeric>
+#include <vector>
+#include <random>
+#include <regex>
+#include <stack>
+#include <string>
 #include <sstream>
 
 // rendering dependend things
@@ -25,6 +25,8 @@
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
+
+#include <imgui.h>
 
 // debug utils
 #include <GLRenderer/RenderDoc/RenderDocTools.h>
