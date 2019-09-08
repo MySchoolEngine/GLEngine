68edbd4eb91a4bc9c1351083c20b7f445d389ecf
Dominik Roháček <RohacekD@gmail.com>
2019-09-08 16:42:21 +0200

    Simplest console implementation possible implemented

---
+++ GLRenderer/GLRenderer/GUI/ConsoleWindow.cpp
@@ -0,0 +1,91 @@
+#include <GLRendererStdafx.h>
+
+#include <GLRenderer/GUI/ConsoleWindow.h>
+
+#include <GLRenderer/GUI/GUIPart.h>
+
+#include <Utils/Logging/Filter.h>
+
+#include <fmt/format.h>
+
+namespace GLEngine::GLRenderer::GUI {
+
+std::array<ImVec4, 4> C_ConsoleWindow::s_LevelColors
+= {
+	ImVec4(1,0,0,1),
+	ImVec4(1,0.8f,0,1),
+	ImVec4(1,1,1,1),
+	ImVec4(0.7f,0.7f,0.7f,1.0)
+};
+
+//=================================================================================
+C_ConsoleWindow::C_ConsoleWindow()
+	: C_Window("Console")
+	, m_LastLogIndex(m_Logs.size()-1)
+{
+
+}
+
+//=================================================================================
+void C_ConsoleWindow::Log(Utils::Logging::E_Level level, Utils::Logging::E_Context context, int line, const char* file, const std::string& text)
+{
+	m_LastLogIndex++;
+	m_LastLogIndex %= m_Logs.size();
+	m_Logs[m_LastLogIndex] = Utils::Logging::S_Data {text, file, line, level, context};
+}
+
+//=================================================================================
+void C_ConsoleWindow::DrawElements() const
+{
+	static const char* levels[] = {"Error", "Warning", "Info", "Debug", "None"};
+	static const char* current_level = levels[4];
+	static int selectedLevelId = 4;
+	using levelUnderlying = std::underlying_type_t<Utils::Logging::E_Level>;
+
+	static std::unique_ptr<Utils::Logging::C_Filter> filter = std::make_unique<Utils::Logging::C_PassAllFilter>();
+
+	if (::ImGui::BeginCombo("##level", current_level))
+	{
+		for (int n = 0; n < sizeof(levels)/sizeof(char*); n++)
+		{
+			bool is_selected = (current_level == levels[n]);
+			if (ImGui::Selectable(levels[n], is_selected))
+			{
+				current_level = levels[n];
+				selectedLevelId = n;
+			}
+			if (is_selected)
+			{
+				ImGui::SetItemDefaultFocus();
+			}
+		}
+		::ImGui::EndCombo();
+	}
+
+
+
+	if (selectedLevelId != 4) {
+		using namespace Utils::Logging;
+		filter = std::make_unique<C_NegationFilter>(new C_LevelFilter(static_cast<E_Level>(selectedLevelId)));
+	}
+	else
+	{
+		filter = std::make_unique<Utils::Logging::C_PassAllFilter>();
+	}
+
+	::ImGui::BeginChild("Output");
+	for (std::size_t i = ((m_LastLogIndex + 1) % m_Logs.size()); i != m_LastLogIndex; i = ((i + 1) % m_Logs.size()))
+	{
+		auto& data = m_Logs[i];
+
+		if (data.m_Text.empty()) {
+			continue;
+		}
+		if (!filter->Filter(data)) continue;
+			::ImGui::TextColored(s_LevelColors[static_cast<levelUnderlying>(data.m_Level)], data.m_Text.c_str());
+	}
+	::ImGui::SetScrollHereY(1.0f);
+	::ImGui::EndChild();
+}
+
+}
---
+++ GLRenderer/GLRenderer/GUI/ConsoleWindow.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <GLRenderer/GUI/Window.h>
+
+#include <Utils/Logging/ILogger.h>
+#include <Utils/Logging/Data.h>
+
+namespace GLEngine::GLRenderer::GUI {
+class C_ConsoleWindow 
+	: public C_Window
+	, public Utils::Logging::I_Logger
+{
+public:
+	C_ConsoleWindow();
+
+	virtual void Log(Utils::Logging::E_Level level, Utils::Logging::E_Context context, int line, const char* file, const std::string& text) override;
+	virtual void DrawElements() const override;
+
+private:
+	//@todo replace with stack vector once we have 'em
+	std::array<Utils::Logging::S_Data, 40>	m_Logs;
+	std::size_t								m_LastLogIndex;
+
+	static std::array<ImVec4, 4>			s_LevelColors;
+};
+
+}
