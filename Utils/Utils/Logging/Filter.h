5f20d3ecea731d9e3cfb72a25a6ed70918b28696
Dominik Roháček <RohacekD@gmail.com>
2019-09-08 16:39:50 +0200

    Added negation filter and export of S_Data

--- Utils/Utils/Logging/Filter.h
+++ Utils/Utils/Logging/Filter.h
@@ -8,11 +8,24 @@
 namespace Utils {
 namespace Logging {
 
-class C_Filter {
+/** ==============================================
+ * @class C_Filter
+ *
+ * @brief	Should be written in that way, that it returns
+ *			true for given set of messages.
+ *
+ * @author 	Dominik Rohacek
+ * Contact: RohacekD@gmail.com
+ * @date 	2019/09/08
+ ** ==============================================*/
+class API_EXPORT C_Filter {
 public:
 	C_Filter(C_Filter* innerFilter = nullptr);
 	virtual ~C_Filter();
-
+	
+	/**
+	 * @return: bool - true if message passed
+	 **/
 	virtual bool Filter(const S_Data& message);
 protected:
 	/** ==============================================
@@ -25,7 +38,7 @@ protected:
 };
 
 //=================================================================================
-class C_ContextFilter : public C_Filter {
+class API_EXPORT C_ContextFilter : public C_Filter {
 public:
 	C_ContextFilter(E_Context context, C_Filter* innerFilter = nullptr);
 	virtual ~C_ContextFilter() = default;
@@ -36,7 +49,7 @@ protected:
 };
 
 //=================================================================================
-class C_LevelFilter : public C_Filter {
+class API_EXPORT C_LevelFilter : public C_Filter {
 public:
 	C_LevelFilter(E_Level context, C_Filter* innerFilter = nullptr);
 	virtual ~C_LevelFilter() = default;
@@ -44,6 +57,25 @@ protected:
 	virtual bool FilterCheck(const S_Data& message) override;
 
 	E_Level m_Level;
+};
+
+//=================================================================================
+class API_EXPORT C_PassAllFilter : public C_Filter {
+public:
+	virtual ~C_PassAllFilter() = default;
+protected:
+	virtual bool FilterCheck(const S_Data& message) override;
+};
+
+//=================================================================================
+class API_EXPORT C_NegationFilter : public C_Filter {
+public:
+	C_NegationFilter(C_Filter* negatedFilter, C_Filter* inner = nullptr);
+	virtual ~C_NegationFilter();
+	virtual bool Filter(const S_Data& message) override;
+private:
+	virtual bool FilterCheck(const S_Data& message) override;
+	C_Filter* m_NegatedFilter;
 };
 
 }
