b5b12a57f5ea21554e3de96b27b99ce8d050319b
RohacekD <RohacekD@gmail.com>
2019-10-28 00:56:11 +0100

    Fixed uninitalised warning in D3D12Window info

--- DX12Renderer/DX12Renderer/D3D12WindowInfo.h
+++ DX12Renderer/DX12Renderer/D3D12WindowInfo.h
@@ -2,18 +2,20 @@
 
 #include <Core/WindowInfo.h>
 
+#include <Core/CoreEnums.h>
 
 namespace GLEngine::DX12Renderer {
 
 //=================================================================================
 struct S_D3D12WindowInfo : public Core::S_WindowInfo {
 	S_D3D12WindowInfo(unsigned int width, unsigned int height)
-		: S_WindowInfo(width, height) {}
+		: S_WindowInfo(width, height)
+		, m_HInstance(nullptr) {}
 
 	HINSTANCE m_HInstance;
 
-	virtual E_Driver GetDriver() const override {
-		return S_WindowInfo::E_Driver::DirectX12;
+	virtual Core::E_Driver GetDriver() const override {
+		return Core::E_Driver::DirectX12;
 	}
 };
 
