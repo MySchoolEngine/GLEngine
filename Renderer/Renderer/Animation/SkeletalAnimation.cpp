18e9077330cddf168f90817436f0b259af8f4a20
RohacekD <RohacekD@gmail.com>
2019-12-30 01:17:39 +0100

    Added initial implementation for animation data structures

--- Renderer/Renderer/Animation/SkeletalAnimation.cpp
+++ Renderer/Renderer/Animation/SkeletalAnimation.cpp
@@ -2,6 +2,81 @@
 
 #include <Renderer/Animation/SkeletalAnimation.h>
 
+#include <Core/CoreMacros.h>
+
 namespace GLEngine::Animation {
 
+//=================================================================================
+// S_Timestamp
+//=================================================================================
+S_Timestamp::S_Timestamp(float timestamp) 
+	: m_Timestamp(S_Timestamp::Clamp(timestamp))
+{
+
+}
+
+//=================================================================================
+S_Timestamp::S_Timestamp() 
+	: m_Timestamp(0.f)
+{
+
+}
+
+//=================================================================================
+float S_Timestamp::Clamp(float f)
+{
+	return std::clamp(f, 0.0f, 1.0f);
+}
+
+//=================================================================================
+bool S_Timestamp::operator>=(const S_Timestamp& other) const
+{
+	return m_Timestamp >= other.m_Timestamp;
+}
+
+//=================================================================================
+// S_SkeletalJointTransform
+//=================================================================================
+S_SkeletalJointTransform::S_SkeletalJointTransform(const glm::mat4& matrix)
+	: m_Transform(matrix[3][0], matrix[3][1], matrix[3][2])
+	, m_Rotation(glm::quat_cast(matrix))
+{
+
+}
+
+//=================================================================================
+// C_SkeletalAnimationKeyframe
+//=================================================================================
+C_SkeletalAnimationKeyframe::C_SkeletalAnimationKeyframe(S_Timestamp timestamp, std::size_t numBones)
+	: m_Timestamp(timestamp)
+{
+	m_Transforms.resize(numBones);
+}
+
+//=================================================================================
+const S_Timestamp& C_SkeletalAnimationKeyframe::GetTimestamp() const
+{
+	return m_Timestamp;
+}
+
+//=================================================================================
+void C_SkeletalAnimationKeyframe::AddJointTransform(std::size_t index, S_SkeletalJointTransform&& transform)
+{
+	m_Transforms[index] = std::move(transform);
+}
+
+//=================================================================================
+// C_SkeletalAnimation
+//=================================================================================
+C_SkeletalAnimation::C_SkeletalAnimation()
+{
+
+}
+
+//=================================================================================
+void C_SkeletalAnimation::AddKeyframe(C_SkeletalAnimationKeyframe&& keyframe)
+{
+	GLE_ASSERT(keyframe.GetTimestamp() >= m_Keyframes.back().GetTimestamp(), "Adding keyframes out of order is not allowed now");
+	m_Keyframes.emplace_back(std::move(keyframe));
+}
 }
--- Renderer/Renderer/Animation/SkeletalAnimation.h
+++ Renderer/Renderer/Animation/SkeletalAnimation.h
@@ -5,26 +5,44 @@
 namespace GLEngine::Animation {
 struct S_Timestamp
 {
-	S_Timestamp() : m_Timestamp(0.f) {}
-	S_Timestamp(float timestamp) : m_Timestamp(S_Timestamp::Clamp(timestamp)) {}
+	S_Timestamp();
+	S_Timestamp(float timestamp);
 
+	bool operator>=(const S_Timestamp& other) const;
 private:
-	static float Clamp(float f) { return std::clamp(f, 0.0f, 1.0f); }
+	static float Clamp(float f);
 	float m_Timestamp;
 };
 
+struct S_SkeletalJointTransform
+{
+	S_SkeletalJointTransform() = default;
+	S_SkeletalJointTransform(const glm::mat4& matrix);
+	glm::vec3 m_Transform;
+	glm::quat m_Rotation;
+};
+
 class C_SkeletalAnimationKeyframe {
+public:
+	C_SkeletalAnimationKeyframe(S_Timestamp timestamp, std::size_t numBones);
 
+	void AddJointTransform(std::size_t index, S_SkeletalJointTransform&& transform);
 
+	const S_Timestamp& GetTimestamp() const;
 private:
-	// should be inbetween 0-1
+	std::vector<S_SkeletalJointTransform> m_Transforms;
 	S_Timestamp m_Timestamp;
 };
 
 class C_SkeletalAnimation {
 public:
-
+	C_SkeletalAnimation();
+	std::vector<S_SkeletalJointTransform> GetTransform(const S_Timestamp timestamp);
+	void AddKeyframe(C_SkeletalAnimationKeyframe&& keyframe);
 private:
+	// ideally BST because intervals do not overlap
+	// but we can use vector for now, as we will usually add 
+	// keyframes in order
 	std::vector<C_SkeletalAnimationKeyframe> m_Keyframes;
 };
 
