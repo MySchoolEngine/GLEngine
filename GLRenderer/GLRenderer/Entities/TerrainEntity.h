7201337dbdd3664c1d129207d3fa7506722b1233
RohacekD <RohacekD@gmail.com>
2020-03-25 00:05:36 +0100

    Better default terrain color

--- GLRenderer/GLRenderer/Entities/TerrainEntity.h
+++ GLRenderer/GLRenderer/Entities/TerrainEntity.h
@@ -42,7 +42,7 @@ public:
 		GUI::Input::C_Slider<int> m_NumSteps	{ 90, 30, 150, "Max lifetime" };
 		GUI::Input::C_Slider<int>	m_PatchSize	{ 40, 4, 60, "Patch size"};
 		GUI::Input::C_CheckBoxValue PerlinNoise	{ true, "Use Perlin noise" };
-		GUI::Input::C_ColorRBGA			m_TerrainColor { "Terrain color", glm::vec4(0.3f, 1.0f, 0.4, 0.0f) };
+		GUI::Input::C_ColorRBGA			m_TerrainColor { "Terrain color", glm::vec4(0.254f, 0.6470f, 0.1921f, 0.0f) };
 		float m_Evaporation		= 0.02f;
 		float m_Gravitation		= 4.0f;
 		float m_InitWater		= 1.0f;
