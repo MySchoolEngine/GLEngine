85a010876285c15d31a3c35ad46a069e655f6b49
RohacekD <RohacekD@gmail.com>
2020-04-04 22:48:38 +0200

    Refactor and cleanup of physics module

--- Physics/Physics/Primitives/AABB.h
+++ Physics/Physics/Primitives/AABB.h
@@ -1,36 +1,94 @@
 #pragma once
 
-#include <Physics/PhysicsApi.h>
+#include <Physics/Primitives/Shapes.h>
 
 #include <glm/glm.hpp>
 
+namespace GLEngine::Physics::Primitives {
 
-namespace GLEngine {
-namespace Physics {
-namespace Primitives {
-
-struct S_Sphere;
-
-struct PHYSICS_API_EXPORT S_AABB {
+struct S_AABB {
 public:
-#pragma warning(push)
-#pragma warning( disable : 4251)
+	constexpr S_AABB()
+	{
+		constexpr float maxFloat = std::numeric_limits<float>::infinity();
+		constexpr float minFloat = -std::numeric_limits<float>::infinity();
+
+		m_Min = glm::vec3(maxFloat, maxFloat, maxFloat);
+		m_Max = glm::vec3(minFloat, minFloat, minFloat);
+	}
+
+	constexpr void Add(const glm::vec3& point)
+	{
+		if (point.x < m_Min.x)
+			m_Min.x = point.x;
+
+		if (point.y < m_Min.y)
+			m_Min.y = point.y;
+
+		if (point.z < m_Min.z)
+			m_Min.z = point.z;
+
+		if (point.x > m_Max.x)
+			m_Max.x = point.x;
+
+		if (point.y > m_Max.y)
+			m_Max.y = point.y;
+
+		if (point.z > m_Max.z)
+			m_Max.z = point.z;
+	}
+	constexpr void Add(const glm::vec4& point)
+	{
+		Add(glm::vec3(point));
+	}
+	constexpr void Add(const S_AABB& bbox)
+	{
+		Add(bbox.m_Max);
+		Add(bbox.m_Min);
+	}
+	constexpr void Add(const S_Sphere& sphere)
+	{
+		const auto pos = sphere.m_position;
+		Add(glm::vec3(pos.x+1, pos.y, pos.z));
+		Add(glm::vec3(pos.x-1, pos.y, pos.z));
+		Add(glm::vec3(pos.x, pos.y+1, pos.z));
+		Add(glm::vec3(pos.x, pos.y-1, pos.z));
+		Add(glm::vec3(pos.x, pos.y, pos.z+1));
+		Add(glm::vec3(pos.x, pos.y, pos.z-1));
+	}
+
+	constexpr void updateWithTriangle(const glm::vec4* triangleVertices)
+	{
+		Add(triangleVertices[0]);
+		Add(triangleVertices[1]);
+		Add(triangleVertices[2]);
+	}
+
+	[[nodiscard]] constexpr S_Sphere GetSphere() const
+	{
+		glm::vec3 center = m_Min + (m_Max - m_Min) / 2.0f;
+		float radius = glm::abs(glm::length(m_Max - center));
+		return S_Sphere(center, radius);
+	}
+	[[nodiscard]] constexpr S_AABB getTransformedAABB(const glm::mat4 matrix) const
+	{
+		S_AABB newBB;
+
+		glm::vec3 size = m_Max - m_Min;
+
+		newBB.Add(glm::vec3(matrix * glm::vec4(m_Min + glm::vec3(size.x, 0.0f, 0.0f), 1.0f)));
+		newBB.Add(glm::vec3(matrix * glm::vec4(m_Min + glm::vec3(size.x, size.y, 0.0f), 1.0f)));
+		newBB.Add(glm::vec3(matrix * glm::vec4(m_Min + glm::vec3(size.x, 0.0f, size.z), 1.0f)));
+		newBB.Add(glm::vec3(matrix * glm::vec4(m_Min + glm::vec3(0.0f, size.y, 0.0f), 1.0f)));
+		newBB.Add(glm::vec3(matrix * glm::vec4(m_Min + glm::vec3(0.0f, 0.0f, size.z), 1.0f)));
+		newBB.Add(glm::vec3(matrix * glm::vec4(m_Min + glm::vec3(0.0f, size.y, size.z), 1.0f)));
+		newBB.Add(glm::vec3(matrix * glm::vec4(m_Min, 1.0f)));
+		newBB.Add(glm::vec3(matrix * glm::vec4(m_Max, 1.0f)));
+
+		return newBB;
+	}
+
 	glm::vec3 m_Min;
 	glm::vec3 m_Max;
-#pragma warning( pop ) 
-	S_AABB();
-
-	void Add(const glm::vec3& point);
-	void Add(const glm::vec4& point);
-	void Add(const S_AABB& bbox);
-	void Add(const S_Sphere& sphere);
-
-
-	void updateWithTriangle(const glm::vec4* triangleVertices);
-
-	S_Sphere GetSphere() const;
-	S_AABB getTransformedAABB(const glm::mat4 matrix) const;
 };
-}
-}
 }
--- Physics/Physics/Primitives/Plane.h
+++ Physics/Physics/Primitives/Plane.h
@@ -1,19 +1,18 @@
 #pragma once
 
-
-#include <Physics/PhysicsApi.h>
+#include <Physics/Primitives/Ray.h>
 
 #include <glm/glm.hpp>
 
-
-namespace GLEngine {
-namespace Physics {
-namespace Primitives {
+namespace GLEngine::Physics::Primitives {
 struct S_Ray;
 
-struct PHYSICS_API_EXPORT S_Plane {
+struct S_Plane {
 	glm::vec4	noraml;
 	float		originOffset;
-	float		Intersect(const S_Ray& ray) const;
+	[[nodiscard]] inline float	Intersect(const S_Ray& ray) const
+	{
+		return -(glm::dot(ray.origin, noraml) - originOffset) / (glm::dot(ray.direction, noraml));
+	}
 };
-}}}
+}
