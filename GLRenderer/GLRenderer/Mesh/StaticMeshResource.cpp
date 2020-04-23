57fbd71e2266661151010bb62c6966c73ed2d3e5
RohacekD <RohacekD@gmail.com>
2020-04-23 12:34:31 +0200

    Static meshes has (bi)tangents

--- GLRenderer/GLRenderer/Mesh/StaticMeshResource.cpp
+++ GLRenderer/GLRenderer/Mesh/StaticMeshResource.cpp
@@ -19,11 +19,15 @@ C_StaticMeshResource::C_StaticMeshResource(const Renderer::MeshData::Mesh & mesh
 	m_VAO.SetBuffer<0, GL_ARRAY_BUFFER>(mesh.vertices);
 	m_VAO.SetBuffer<1, GL_ARRAY_BUFFER>(mesh.normals);
 	m_VAO.SetBuffer<2, GL_ARRAY_BUFFER>(mesh.texcoords);
+	m_VAO.SetBuffer<3, GL_ARRAY_BUFFER>(mesh.tangent);
+	m_VAO.SetBuffer<4, GL_ARRAY_BUFFER>(mesh.bitangent);
 
 
 	m_VAO.EnableArray<0>();
 	m_VAO.EnableArray<1>();
 	m_VAO.EnableArray<2>();
+	m_VAO.EnableArray<3>();
+	m_VAO.EnableArray<4>();
 
 	m_VAO.unbind();
 }
