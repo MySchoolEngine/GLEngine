a11a771579ccfa631236a36554c09e09691c191e
RohacekD <RohacekD@gmail.com>
2020-06-18 13:38:26 +0200

    Left over from previous

--- GLRenderer/GLRenderer/GLFW/GLFWoGLWindow.h
+++ GLRenderer/GLRenderer/GLFW/GLFWoGLWindow.h
@@ -19,7 +19,7 @@ namespace GLRenderer::GLFW {
 class C_GLFWoGLWindow : public GLFWManager::C_GLFWWindow {
 public:
 	C_GLFWoGLWindow(const Core::S_WindowInfo& wndInfo);
-	virtual ~C_GLFWoGLWindow() = default;
+	virtual ~C_GLFWoGLWindow();
 
 	virtual const std::unique_ptr<Renderer::I_Renderer>& GetRenderer() const override;
 protected:
