e25b9208434a324e589b8cd249755cd9b1923c9e
RohacekD <RohacekD@gmail.com>
2020-07-08 19:32:42 +0200

    STL std afx moved to separate file in order to unify them

--- Core/CoreStdafx.h
+++ Core/CoreStdafx.h
@@ -2,14 +2,7 @@
 
 #include <Utils/Logging/LoggingMacros.h>
 
-#include <iostream>
-#include <memory>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <functional>
-#include <stack>
-#include <iterator>
+#include <Utils/STLAfx.h>
 
 #include <Core/GUID.h>
 
--- DX12Renderer/DX12RendererStdafx.h
+++ DX12Renderer/DX12RendererStdafx.h
@@ -2,23 +2,7 @@
 
 #include <Utils/Logging/LoggingMacros.h>
 
-#include <algorithm>
-#include <array>
-#include <chrono>
-#include <exception>
-#include <functional>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <memory>
-#include <map>
-#include <numeric>
-#include <vector>
-#include <random>
-#include <regex>
-#include <stack>
-#include <string>
-#include <sstream>
+#include <Utils/STLAfx.h>
 
 // rendering dependend things
 #include <imgui.h>
