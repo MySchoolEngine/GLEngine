a875d07d3b44c4d4e59d5a61beaccafe46ed268f
Dominik Roháček <RohacekD@users.noreply.github.com>
2020-07-08 23:26:08 +0200

    Update ComponentBuilderFactory.h
    Adding nodiscard into component builder factory

--- GLRenderer/GLRenderer/Components/ComponentBuilderFactory.h
+++ GLRenderer/GLRenderer/Components/ComponentBuilderFactory.h
@@ -7,7 +7,7 @@ namespace GLEngine::GLRenderer::Components {
 class C_ComponentBuilderFactory : public Entity::I_ComponentBuilderFactory
 {
 public:
-	virtual std::unique_ptr<Entity::I_ComponenetBuilder> GetFactory(const std::string& name) override;
+	[[nodiscard]] virtual std::unique_ptr<Entity::I_ComponenetBuilder> GetFactory(const std::string& name) override;
 	void ConstructFromFile(std::shared_ptr<Entity::I_Entity> entity, const std::filesystem::path& file) override;
 
 };
