d80646d5376e90dd0af7eeb23f2b166c22bf6286
RohacekD <RohacekD@gmail.com>
2020-07-08 19:59:01 +0200

    nodiscard for scene loader

--- Renderer/Renderer/Mesh/Loading/SceneLoader.h
+++ Renderer/Renderer/Mesh/Loading/SceneLoader.h
@@ -11,6 +11,6 @@ class RENDERER_API_EXPORT SceneLoader
 public:
 	//filepath - the folder where the model is
 	//filename - the actual name of the file
-	bool addModelFromFileToScene(const std::filesystem::path& filepath, const std::filesystem::path& filename, std::shared_ptr<Renderer::MeshData::Scene> scene, const glm::mat4& transform = glm::mat4(1));
+	[[nodiscard]] bool addModelFromFileToScene(const std::filesystem::path& filepath, const std::filesystem::path& filename, std::shared_ptr<Renderer::MeshData::Scene> scene, const glm::mat4& transform = glm::mat4(1));
 };
 }
