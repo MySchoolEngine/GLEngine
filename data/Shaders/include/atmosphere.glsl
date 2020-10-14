78ea99407afd8aa29c16bc41fa71bbe45f28b059
RohacekD <RohacekD@gmail.com>
2020-10-14 17:11:07 +0200

    A lot of fixes on atmospheric rendering

--- data/Shaders/include/atmosphere.glsl
+++ data/Shaders/include/atmosphere.glsl
@@ -1,11 +1,17 @@
 // for 680, 550, 440
 const vec3 SeaLevelScatterCoef = vec3(5.8, 13.5, 33.1) * pow(10.0, -6);
-const float Hr = 1/8000; // Hr = 8 km
-const float Hm = 1/1200; // Hm = 1.2 km
+const vec3 SeaLevelMScatterCoef = vec3(21) * pow(10,-6);
+const float Hr = 1/7.994e3; // Hr = 8 km
+const float Hm = 1/1.2e3; // Hm = 1.2 km
 const float scatter2ExtinctionRatio = 0.9; // B^s/B^e = 0.9 according to paper, only areosols absorbs incident light
 
 const float earthRadius = 6378*1000; // 6378 setri se osle
 const float atmosphereThickness = 60 * 1000; // 60km atmosphere
+
+// 695500 km
+const float sunRadius = 695500e3;
+// AU = 149597870700 km;
+const float sunDistance = 1496e8;
 
 vec3 GetPlanetCenter()
 {
@@ -22,8 +28,22 @@ Sphere GetAtmosphereBoundary()
 
 Sphere GetSunSphere()
 {
+	vec3 camPosition = frame.CameraPosition.xyz/frame.CameraPosition.w;
+	camPosition.y = 0;
 	const vec3 planetCenter = GetPlanetCenter();
-    return Sphere(planetCenter + normalize(frame.SunPos) * (earthRadius), 150000.0); 
+    return Sphere(camPosition + normalize(frame.SunPos) * (earthRadius), 150000.0); 
+}
+
+vec3 GetSunColor(Ray r)
+{
+	const float sunMulti = 1;
+	const float sunBorder = asin(sunRadius / sunDistance);
+	const float angle = acos(dot(r.dir, normalize(frame.SunPos)));
+	if(angle <= sunBorder * sunMulti)
+	{
+		return vec3(1.0, 1.0, 1.0) * 13.61839144264511 * (1 - smoothstep(sunBorder, sunBorder * sunMulti, angle));
+	} 
+	return vec3(0);
 }
 
 float GetAltitude(vec3 point)
@@ -43,7 +63,7 @@ vec3 GetAirExtinctionCoef(float altitude)
 
 vec3 GetAreosolAbsorbtionCoef(float altitude)
 {
-	return SeaLevelScatterCoef * exp(- altitude * Hm);
+	return SeaLevelMScatterCoef * exp(- altitude * Hm);
 }
 
 vec3 GetAreosolExtinctionCoef(float altitude)
@@ -104,23 +124,37 @@ float MiePhaseFunctionXLF(float g, float angle)
 vec3 InScatteredLight(vec3 y, vec3 v, vec3 s)
 {
 	const float altitude = GetAltitude(y); 
-	float Mie = MiePhaseFunctionCS(0.2, dot(v, s));
-	return GetAirExtinctionCoef(GetAltitude(y)) * ReyleighPhaseFunction(dot(v, s)) + GetAreosolExtinctionCoef(GetAltitude(y)) * MiePhaseFunctionCS(1, dot(v, s));
+	float Mie = MiePhaseFunctionXLF(0.6, dot(v, s));
+	return GetAirExtinctionCoef(GetAltitude(y)) * ReyleighPhaseFunction(dot(v, s)) + 
+		GetAreosolExtinctionCoef(GetAltitude(y)) * Mie;
 }
 
 vec3 GetSkyRadiance(Ray r, float rayLen)
 {
 	const Sphere sun = GetSunSphere();
-	const int numSamples = 1000;
+	const int numSamples = 16;
 	vec3 samplingPoint = r.origin;
 	float stepSize = rayLen / (numSamples - 1);
+
+	vec3 airMassAlongARay = vec3(0);
 
 	vec3 radiance = vec3(0, 0, 0);
 	for ( int i = 0; i < numSamples; ++i)
 	{
-		radiance += stepSize * InScatteredLight(samplingPoint, r.dir, normalize(sun.center - samplingPoint)) * Transmittance(r, i * stepSize);
+		const float altitude = GetAltitude(samplingPoint); 
+		const vec3 BeR = GetAirExtinctionCoef(altitude);
+		const vec3 BeM = GetAreosolExtinctionCoef(altitude);
+		airMassAlongARay += stepSize * (BeR + BeM);
+
+		vec3 intersect;
+		const Ray toSun = Ray(samplingPoint, normalize(sun.center - samplingPoint));
+		Intersect(toSun, sun, intersect);
+
+		vec3 toSunTransmittance = Transmittance(toSun, intersect.z);
+
+		radiance += stepSize * InScatteredLight(samplingPoint, r.dir, normalize(sun.center - samplingPoint)) * exp(-airMassAlongARay) * toSunTransmittance * vec3(1.0, 1.0, 1.0) * 13.61839144264511; // * Transmittance(toSun, intersect.z);
 		samplingPoint += r.dir * stepSize;
 	}
 
-	return radiance;
+	return radiance + exp(-airMassAlongARay) * GetSunColor(r);// Transmittance(r, rayLen);
 }
