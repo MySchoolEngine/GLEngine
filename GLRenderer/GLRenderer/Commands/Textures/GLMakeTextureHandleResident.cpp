c116018d62d38affc7a9f5e5f1d905682cbefb4e
RohacekD <RohacekD@gmail.com>
2020-11-24 01:53:52 +0100

    Afx -> afx (wrong case)

--- GLRenderer/GLRenderer/Commands/Textures/GLMakeTextureHandleResident.cpp
+++ GLRenderer/GLRenderer/Commands/Textures/GLMakeTextureHandleResident.cpp
@@ -1,4 +1,4 @@
-#include <GLRendererStdAfx.h>
+#include <GLRendererStdafx.h>
 
 #include <GLRenderer/Commands/Textures/GLMakeTextureHandleResident.h>
 
--- Renderer/Renderer/Animation/ColladaLoading/ColladaLoader.cpp
+++ Renderer/Renderer/Animation/ColladaLoading/ColladaLoader.cpp
@@ -1,4 +1,4 @@
-#include <RendererStdAfx.h>
+#include <RendererStdafx.h>
 
 #include <Renderer/Animation/ColladaLoading/ColladaLoader.h>
 
