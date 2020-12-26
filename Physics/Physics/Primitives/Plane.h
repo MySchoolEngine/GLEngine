9ab84ac328a5d8b91fe34c1213f326716ebc8d69
RohacekD <RohacekD@gmail.com>
2020-12-26 17:33:57 +0100

    Improved plane definition and sphere intersection
    (With backward compatibility)

--- Physics/Physics/Primitives/Plane.h
+++ Physics/Physics/Primitives/Plane.h
@@ -10,14 +10,22 @@ namespace GLEngine::Physics::Primitives {
 struct S_Ray;
 
 struct S_Plane : public T_Intersectable<S_Plane> {
-	constexpr S_Plane(glm::vec4 _normal, float offset)
+	constexpr S_Plane(glm::vec3 _normal, float offset)
 		: normal(_normal)
-		, originOffset(offset) {}
-	glm::vec4	normal;
-	float		originOffset;
+		, origin(normal* offset) {}
+	constexpr S_Plane(glm::vec3 _normal, glm::vec3 origin)
+		: normal(_normal)
+		, origin(origin) {}
+	glm::vec3	normal;
+	glm::vec3	origin;
 	[[nodiscard]] inline constexpr float	IntersectImpl(const S_Ray& ray) const
 	{
-		return -(glm::dot(glm::vec4(ray.origin, 1.0f), normal) - originOffset) / (glm::dot(ray.direction, glm::vec3(normal.x, normal.y, normal.z)));
+		if (glm::dot(ray.direction, normal) > 0.0)
+		{
+			return -1;
+		}
+
+		return (glm::dot(origin - ray.origin, normal)) / (glm::dot(ray.direction, normal));
 	}
 };
 }
--- Physics/Physics/Primitives/Shapes.h
+++ Physics/Physics/Primitives/Shapes.h
@@ -11,6 +11,10 @@
  
 #pragma once
 
+#include <Physics/Primitives/Intersectable.h>
+
+#define GLM_GTX_intersect
+#include <glm/gtx/intersect.hpp>
 #include <glm/glm.hpp>
 
 namespace GLEngine::Physics::Primitives {
@@ -23,13 +27,24 @@ namespace GLEngine::Physics::Primitives {
 * Contact:	RohacekD@gmail.com
 * @date 	2018/05/16
 ** ==============================================*/
-struct S_Sphere {
+struct S_Sphere : public T_Intersectable<S_Sphere> {
 public:
 	S_Sphere() = default;
 	constexpr S_Sphere(const glm::vec3& position, float radius)
 		: m_position(position)
 		, m_radius(radius)
 	{}
+
+
+	[[nodiscard]] inline float	IntersectImpl(const S_Ray& ray) const
+	{
+		float t = 0.0f;
+		if (glm::intersectRaySphere(ray.origin, ray.direction, m_position, m_radius * m_radius, t))
+		{
+			return t;
+		}
+		return -1.f;
+	}
 
 	[[nodiscard]] bool IsColliding(const S_Sphere& other) const
 	{
