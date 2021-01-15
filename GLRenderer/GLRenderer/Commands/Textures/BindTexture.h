9847a452f42697e16bdc37bc6534d26607fec635
RohacekD <RohacekD@gmail.com>
2021-01-15 21:39:18 +0100

    Bind texture command

---
+++ GLRenderer/GLRenderer/Commands/Textures/BindTexture.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Renderer/IRenderCommand.h>
+
+namespace GLEngine::GLRenderer::Commands {
+class C_GLBindTexture final : public Renderer::I_RenderCommand {
+public:
+	explicit C_GLBindTexture(GLenum target, GLuint texture);
+	static C_GLBindTexture UnbindTarget(GLenum target);
+
+	//=================================================================================
+	virtual void Commit() override;
+	virtual E_Type GetType() const override;
+	virtual std::shared_ptr<Renderer::I_Resource> GetResource() const override
+	{
+		return nullptr;
+	}
+
+	//=================================================================================
+	[[nodiscard]] virtual std::string GetDescriptor() const override;
+
+private:
+	GLenum m_Target;
+	GLuint m_Texture;
+};
+
+}
