dc8bcb2ce18bb399b8d0b99c9d9d91b2e67360fb
RohacekD <RohacekD@gmail.com>
2021-01-15 19:58:37 +0100

    Improved camera ray generation

--- Renderer/Renderer/Cameras/FreelookCamera.h
+++ Renderer/Renderer/Cameras/FreelookCamera.h
@@ -63,6 +63,7 @@ public:
 	//=============================================================
 	// Renderer::I_CameraComponent
 	//=============================================================
+	virtual glm::mat4 GetScreenToworldMatrix() const override;
 	virtual glm::mat4 GetViewProjectionMatrix() const override;
 	virtual glm::mat4 GetProjectionMatrix() const override;
 	virtual glm::mat4 GetViewMatrix() const override;
@@ -144,5 +145,6 @@ private:
 
 	glm::mat4						_viewMatrix;
 	glm::mat4						_projectionMatrix;
+	glm::mat4						_ScreenToWorld;
 };
 }}
