205ab9c22054a8c49637c0b4364ad54d2eac2eec
RohacekD <RohacekD@gmail.com>
2021-01-28 00:28:05 +0100

    Adding raytracing reflection models

---
+++ Renderer/Renderer/RayCasting/ReflectionModels/IReflectionModel.cpp
@@ -0,0 +1,36 @@
+#include <RendererStdafx.h>
+
+#include <Renderer/RayCasting/ReflectionModels/IReflectionModel.h>
+
+
+namespace GLEngine::Renderer
+{
+
+//=================================================================================
+float I_ReflectionModel::FresnelDieletrics(float cosThetaI, float etaI, float etaO)
+{
+	const auto entering = cosThetaI > 0.f;
+	if (entering)
+	{
+		std::swap(etaI, etaO);
+		cosThetaI = std::abs(cosThetaI);
+	}
+
+	const auto sinThetaI = std::sqrt(std::max(0.f, 1 - cosThetaI * cosThetaI));
+	// literally Snell's law
+	const auto sinThetaO = etaI / etaO * sinThetaI;
+
+	// total internal reflection
+	if (sinThetaO >= 1.f)
+	{
+		return 1.f;
+	}
+
+	const auto cosThetaO = std::sqrt(std::max(0.f, 1 - cosThetaI * cosThetaI));
+
+	const auto Rparallel = ((etaO * cosThetaI) - (etaI * cosThetaO)) / ((etaO * cosThetaI) + (etaI * cosThetaO));
+	const auto Rperpendicular = ((etaI * cosThetaI) - (etaO * cosThetaO)) / ((etaI * cosThetaI) + (etaO * cosThetaO));
+	return (Rparallel * Rparallel + Rperpendicular * Rperpendicular) / 2;
+}
+
+}
---
+++ Renderer/Renderer/RayCasting/ReflectionModels/SpecularReflection.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Renderer/RayCasting/ReflectionModels/IReflectionModel.h>
+
+
+// so far I implemented dielectric reflection, so all I need is to know the eta of
+// current media and the media around the border of medias
+namespace GLEngine::Renderer {
+
+struct S_Frame;
+
+class C_SpecularReflection : public I_ReflectionModel
+{
+public:
+	C_SpecularReflection(float etaI, float etaO);
+
+	virtual glm::vec3 f(const glm::vec3& wi, const glm::vec3& wo) const override;
+	virtual glm::vec3 SampleF(const glm::vec3& wi, glm::vec3& wo, const S_Frame& frame, const glm::vec2& rng, float* pdf) const override;
+	virtual float Pdf(const glm::vec3& wi, const glm::vec3& wo) const override;
+private:
+	float m_etaI;
+	float m_etaO;
+};
+}
+
