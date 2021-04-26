3ce6bf6cc9f925d21c69cf0f4f12e76574311e26
RohacekD <RohacekD@gmail.com>
2021-04-26 11:36:21 +0200

    Basic curve implementation

---
+++ Renderer/Renderer/Mesh/Curve.cpp
@@ -0,0 +1,51 @@
+#include <RendererStdafx.h>
+
+#include <Renderer/Mesh/Curve.h>
+
+
+namespace GLEngine::Renderer {
+
+//=================================================================================
+C_Curve::C_Curve()
+{
+}
+
+//=================================================================================
+void C_Curve::AddControlPoint(const std::size_t position, const glm::vec3& point)
+{
+	auto it = m_Points.begin();
+	std::advance(it, position);
+	m_Points.insert(it, point);
+}
+
+//=================================================================================
+void C_Curve::ForEachControlPoint(const T_ControlPointFn& fn)
+{
+	std::for_each(m_Points.begin(), m_Points.end(), fn);
+}
+
+//=================================================================================
+void C_Curve::SetControlPoint(const std::size_t position, const glm::vec3& point)
+{
+	auto it = m_Points.begin();
+	std::advance(it, position);
+	*it = point;
+}
+
+//=================================================================================
+void C_Curve::RemoveControlPoint(const std::size_t position)
+{
+	auto it = m_Points.begin();
+	std::advance(it, position);
+	m_Points.erase(it);
+}
+
+//=================================================================================
+glm::vec3 C_Curve::GetControlPoint(const std::size_t position)
+{
+	auto it = m_Points.begin();
+	std::advance(it, position);
+	return *it;
+}
+
+}
---
+++ Renderer/Renderer/Mesh/Curve.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Renderer/RendererApi.h>
+
+namespace GLEngine::Renderer {
+// this class is only responsible for storage of curve data
+class RENDERER_API_EXPORT C_Curve {
+public:
+	C_Curve();
+	void AddControlPoint(const std::size_t position, const glm::vec3& point);
+	void SetControlPoint(const std::size_t position, const glm::vec3& point);
+	void RemoveControlPoint(const std::size_t position);
+	[[nodiscard]] glm::vec3 GetControlPoint(const std::size_t position);
+
+	using T_ControlPointFn = std::function<void(glm::vec3)>;
+	void ForEachControlPoint(const T_ControlPointFn& fn);
+private:
+	std::vector<glm::vec3> m_Points;
+};
+} // namespace GLEngine::Renderer
