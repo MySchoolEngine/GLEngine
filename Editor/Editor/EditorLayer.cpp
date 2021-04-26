19d68e704efd7fc28fd36100ba6b9e1e917babff
RohacekD <RohacekD@gmail.com>
2021-04-26 13:01:16 +0200

    Make editor layer propper layer not only event reciever

--- Editor/Editor/EditorLayer.cpp
+++ Editor/Editor/EditorLayer.cpp
@@ -9,9 +9,22 @@
 namespace GLEngine::Editor {
 
 //=================================================================================
-C_EditorLayer::C_EditorLayer(Renderer::I_DebugDraw& dd)
-	: m_dd(dd)
+C_EditorLayer::C_EditorLayer(Renderer::I_DebugDraw& dd, Core::I_Input& directInput)
+	: Core::C_Layer("Editor Layer")
+	, m_dd(dd)
+	, m_Input(directInput)
 {
 }
 
-};
+//=================================================================================
+void C_EditorLayer::OnEvent(Core::I_Event& event)
+{
+}
+
+//=================================================================================
+void C_EditorLayer::OnUpdate()
+{
+
+}
+
+}; // namespace GLEngine::Editor
--- Editor/Editor/EditorLayer.h
+++ Editor/Editor/EditorLayer.h
@@ -2,23 +2,31 @@
 
 #include <Editor/EditorApi.h>
 
-#include <Core/EventSystem/EventReciever.h>
+#include <Core/EventSystem/Layer.h>
 
 namespace GLEngine::Renderer {
 class I_Renderer;
 class I_DebugDraw;
 } // namespace GLEngine::Renderer
+namespace GLEngine::Core {
+class I_Input;
+} // namespace GLEngine::Core
 
 // This class will be main point of editor communication
 // it should decide which element will receive event
 // hold gizmos manager, know about selected 3D entities
 // decide which model will render its AABB box etc.
 namespace GLEngine::Editor {
-class EDITOR_API_EXPORT C_EditorLayer : public Core::I_EventReciever {
+class EDITOR_API_EXPORT C_EditorLayer : public Core::C_Layer {
 public:
-	C_EditorLayer(Renderer::I_DebugDraw& dd);
+	C_EditorLayer(Renderer::I_DebugDraw& dd, Core::I_Input& directInput);
+
+	virtual void OnEvent(Core::I_Event& event) override;
+	virtual void OnUpdate() override;
+
 
 private:
 	Renderer::I_DebugDraw& m_dd;
+	Core::I_Input&		   m_Input
 };
 } // namespace GLEngine::Editor
