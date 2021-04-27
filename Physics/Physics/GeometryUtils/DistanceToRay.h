da2c1035c83b1cd471f19a25db1f5acf92f94bb1
RohacekD <RohacekD@gmail.com>
2021-04-27 18:53:19 +0200

    Fix ray to point distance

--- Physics/Physics/GeometryUtils/DistanceToRay.h
+++ Physics/Physics/GeometryUtils/DistanceToRay.h
@@ -7,15 +7,14 @@
 namespace GLEngine::Physics {
 [[nodiscard]] float DistanceToRay(const glm::vec3& point, const Primitives::S_Ray& ray)
 {
-	const auto		LineLength	  = glm::distance(ray.origin, ray.origin + ray.direction);
 	const glm::vec3 Vector		  = point - ray.origin;
-	const glm::vec3 LineDirection = ray.direction;
+	const glm::vec3 LineDirection = ray.direction; // should be normalized
 
 	// Project Vector to LineDirection to get the distance of point from ray.origin
 	auto Distance = glm::dot(Vector, LineDirection);
 
 	if (Distance <= 0.0f)
 		return glm::distance(ray.origin, point);
-	return Distance;
+	return glm::distance(point, ray.origin + ray.direction * Distance); // TODO optimise
 }
 } // namespace GLEngine::Physics
