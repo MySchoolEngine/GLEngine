43e13c7f0b38c27d920176d122818569fd70871e
RohacekD <RohacekD@gmail.com>
2021-04-27 20:39:04 +0200

    Add colours definitions

---
+++ Renderer/Renderer/Colours.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace GLEngine::Colours {
+
+inline const static auto white	= glm::vec3(1, 1, 1);
+inline const static auto red	= glm::vec3(1, 0, 0);
+inline const static auto green	= glm::vec3(0, 1, 0);
+inline const static auto blue	= glm::vec3(0, 0, 1);
+inline const static auto purple = glm::vec3(1, 0, 1);
+inline const static auto cyan	= glm::vec3(0, 1, 1);
+inline const static auto gray	= glm::vec3(169.0 / 255.0, 169.0 / 255.0, 169.0 / 255.0);
+
+} // namespace GLEngine::Colours
