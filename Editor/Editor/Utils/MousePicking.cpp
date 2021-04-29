af1763a8bde96e788af30f7d56eefe84e3f897a4
RohacekD <RohacekD@gmail.com>
2021-04-30 00:03:23 +0200

    Introduce mouse picking helper
    Serves situations when more than one element can be mouse picked

--- Editor/Editor/Utils/MousePicking.cpp
+++ Editor/Editor/Utils/MousePicking.cpp
@@ -4,6 +4,8 @@
 
 #include <Renderer/ICameraComponent.h>
 
+#include <Core/Input.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtx/closest_point.hpp>
 
@@ -34,6 +36,55 @@ float ScreenSpaceDistanceToLine(const glm::vec3& a, const glm::vec3& b, const gl
 	const auto clipSpaceB = glm::vec2(BinFrustum.x, BinFrustum.y) / BinFrustum.w;
 
 	return glm::distance(glm::closestPointOnLine(mousePosition, clipSpaceA, clipSpaceB), mousePosition);
+}
+
+//=================================================================================
+// C_MousePickingHelper
+//=================================================================================
+C_MousePickingHelper::C_MousePickingHelper(const Core::I_Input& input, const Renderer::I_CameraComponent& camera)
+	: m_Input(input)
+	, m_Camera(camera)
+{
+}
+
+//=================================================================================
+void C_MousePickingHelper::LineSegment(const glm::vec3& a, const glm::vec3& b, const T_Callback& cb)
+{
+	const auto mousePosition = m_Input.GetClipSpaceMouseCoord();
+	const auto distance		 = ScreenSpaceDistanceToLine(a, b, mousePosition, m_Camera);
+	if (distance < .1f) // todo should be more tight to pixels than size of clip space
+	{
+		m_Interactions.emplace_back(distance, 0.0f, cb);
+	}
+}
+
+//=================================================================================
+void C_MousePickingHelper::Point(const glm::vec3& point, const T_Callback& cb)
+{
+	// TODO DEPTH
+	const auto mousePosition = m_Input.GetClipSpaceMouseCoord();
+	const auto distance		 = ScreenSpaceDistance(point, mousePosition, m_Camera);
+	if (distance < .1f) // todo should be more tight to pixels than size of clip space
+	{
+		m_Interactions.emplace_back(distance, 0.0f, cb);
+	}
+}
+
+//=================================================================================
+bool C_MousePickingHelper::SelectInteraction()
+{
+	std::sort(m_Interactions.begin(), m_Interactions.end(), [](const auto a, const auto b) {
+		if (std::abs(a.distance - b.distance) <= 0.01f)
+		{
+			return a.depth < b.depth;
+		}
+		return a.distance < b.distance;
+	});
+	if (m_Interactions.empty())
+		return false;
+
+	m_Interactions.begin()->cb();
+	return true;
 }
 
 } // namespace GLEngine::Editor
--- Editor/Editor/Utils/MousePicking.h
+++ Editor/Editor/Utils/MousePicking.h
@@ -1,5 +1,8 @@
 #pragma once
 
+namespace GLEngine::Core {
+class I_Input;
+}
 namespace GLEngine::Renderer {
 class I_CameraComponent;
 } // namespace GLEngine::Renderer
@@ -7,4 +10,35 @@ class I_CameraComponent;
 namespace GLEngine::Editor {
 [[nodiscard]] float ScreenSpaceDistance(const glm::vec3& point, const glm::vec2& mousePosition, const Renderer::I_CameraComponent& camera);
 [[nodiscard]] float ScreenSpaceDistanceToLine(const glm::vec3& a, const glm::vec3& b, const glm::vec2& mousePosition, const Renderer::I_CameraComponent& camera);
+
+class C_MousePickingHelper {
+public:
+	C_MousePickingHelper(const Core::I_Input& input, const Renderer::I_CameraComponent& camera);
+
+	// will be called when the interaction will be selected
+	using T_Callback = std::function<void()>;
+	void LineSegment(const glm::vec3& a, const glm::vec3& b, const T_Callback& cb);
+	void Point(const glm::vec3& point, const T_Callback& cb);
+
+	// return true if any interaction have been selected
+	bool SelectInteraction();
+
+private:
+	const Core::I_Input&			   m_Input;
+	const Renderer::I_CameraComponent& m_Camera;
+
+	// some structure with distance, depth and interactionID
+	struct Interaction {
+		Interaction(float _distance, float _depth, const T_Callback& _cb)
+			: distance(_distance)
+			, depth(_depth)
+			, cb(_cb)
+		{
+		}
+		float	   distance;
+		float	   depth;
+		T_Callback cb;
+	};
+	std::vector<Interaction> m_Interactions;
+};
 } // namespace GLEngine::Editor
