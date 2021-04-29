92e0717a7fd708bf18c5f507a7b267cd4273740e
RohacekD <RohacekD@gmail.com>
2021-04-30 00:04:17 +0200

    Basics for gizmo controlling

--- Editor/Editor/Gizmo.cpp
+++ Editor/Editor/Gizmo.cpp
@@ -7,6 +7,9 @@
 #include <Renderer/DebugDraw.h>
 #include <Renderer/ICameraComponent.h>
 
+#include <Core/EventSystem/Event/KeyboardEvents.h>
+#include <Core/EventSystem/Event/MouseEvents.h>
+#include <Core/EventSystem/EventDispatcher.h>
 #include <Core/Input.h>
 
 namespace GLEngine::Editor {
@@ -14,6 +17,7 @@ namespace GLEngine::Editor {
 //=================================================================================
 C_Gizmo::C_Gizmo(const glm::vec3& position)
 	: m_Position(position)
+	, m_ControllingGizmo(false)
 {
 }
 
@@ -32,6 +36,10 @@ void C_Gizmo::SetPosition(const glm::vec3& position)
 //=================================================================================
 void C_Gizmo::OnUpdate(const Core::I_Input& input, const Renderer::I_CameraComponent& camera)
 {
+	// do not change mouse over when dragging gizmo
+	if (m_ControllingGizmo)
+		return;
+
 	std::array<float, 3> closestLines;
 
 	const auto mosuePosition = input.GetClipSpaceMouseCoord();
@@ -59,6 +67,57 @@ void C_Gizmo::Draw(Renderer::I_DebugDraw& dd) const
 bool C_Gizmo::IsMouseOverGizmo() const
 {
 	return m_MouseOverDirection.has_value();
+}
+
+//=================================================================================
+void C_Gizmo::OnEvent(Core::I_Event& event)
+{
+	Core::C_EventDispatcher d(event);
+	d.Dispatch<Core::C_MouseButtonPressed>(std::bind(&C_Gizmo::OnMouseKeyPressed, this, std::placeholders::_1));
+	d.Dispatch<Core::C_MouseButtonReleased>(std::bind(&C_Gizmo::OnMouseKeyReleased, this, std::placeholders::_1));
+	d.Dispatch<Core::C_MouseMoved>(std::bind(&C_Gizmo::OnMouseMoved, this, std::placeholders::_1));
+	d.Dispatch<Core::C_KeyPressedEvent>(std::bind(&C_Gizmo::OnKeyPressed, this, std::placeholders::_1));
+}
+
+//=================================================================================
+bool C_Gizmo::OnMouseKeyPressed(Core::C_MouseButtonPressed& event)
+{
+	if (IsMouseOverGizmo())
+	{
+		m_ControllingGizmo = true;
+		return true;
+	}
+	return false;
+}
+
+//=================================================================================
+bool C_Gizmo::OnMouseKeyReleased(Core::C_MouseButtonReleased& event)
+{
+	if (m_ControllingGizmo)
+	{
+		m_ControllingGizmo = false;
+		return true;
+	}
+	return false;
+}
+
+//=================================================================================
+bool C_Gizmo::OnKeyPressed(Core::C_KeyPressedEvent& event)
+{
+	return false;
+}
+
+//=================================================================================
+bool C_Gizmo::OnMouseMoved(Core::C_MouseMoved& event)
+{
+	if (m_ControllingGizmo == false)
+		return false;
+
+	//todo:
+	// 1) Reproject to the world
+	// 2) Find the offset
+	// 3) Move the position
+	return true;
 }
 
 } // namespace GLEngine::Editor
--- Editor/Editor/Gizmo.h
+++ Editor/Editor/Gizmo.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <Core/EventSystem/EventReciever.h>
+
 namespace GLEngine::Core {
 class I_Input;
-}
+class C_MouseButtonPressed;
+class C_MouseButtonReleased;
+class C_MouseMoved;
+class C_KeyPressedEvent;
+} // namespace GLEngine::Core
 
 namespace GLEngine::Renderer {
 class I_DebugDraw;
@@ -10,19 +16,26 @@ class I_CameraComponent;
 } // namespace GLEngine::Renderer
 
 namespace GLEngine::Editor {
-class C_Gizmo {
+class C_Gizmo : Core::I_EventReciever {
 public:
 	C_Gizmo(const glm::vec3& position);
 
-	glm::vec3 GetPosition() const;
-	void	  SetPosition(const glm::vec3& position);
+	[[nodiscard]] glm::vec3 GetPosition() const;
+	void					SetPosition(const glm::vec3& position);
 
 	void OnUpdate(const Core::I_Input& input, const Renderer::I_CameraComponent& camera);
 	void Draw(Renderer::I_DebugDraw& dd) const;
 
 	[[nodiscard]] bool IsMouseOverGizmo() const;
 
+	void OnEvent(Core::I_Event& event) override;
+
 private:
+	[[nodiscard]] bool OnMouseKeyPressed(Core::C_MouseButtonPressed& event);
+	[[nodiscard]] bool OnMouseKeyReleased(Core::C_MouseButtonReleased& event);
+	[[nodiscard]] bool OnMouseMoved(Core::C_MouseMoved& event);
+	[[nodiscard]] bool OnKeyPressed(Core::C_KeyPressedEvent& event);
+
 	enum class E_Direction
 	{
 		Right,
@@ -31,5 +44,6 @@ private:
 	};
 	glm::vec3				   m_Position;
 	std::optional<E_Direction> m_MouseOverDirection;
+	bool					   m_ControllingGizmo;
 };
 } // namespace GLEngine::Editor
