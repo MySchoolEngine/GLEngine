0281ab6ccb05a05835711c7dc093b3eeecd58e32
RohacekD <RohacekD@gmail.com>
2021-05-08 17:23:55 +0200

    Gizmo controls

--- Editor/Editor/Gizmo.cpp
+++ Editor/Editor/Gizmo.cpp
@@ -7,6 +7,8 @@
 #include <Renderer/DebugDraw.h>
 #include <Renderer/ICameraComponent.h>
 
+#include <Physics/Primitives/Plane.h>
+
 #include <Core/EventSystem/Event/KeyboardEvents.h>
 #include <Core/EventSystem/Event/MouseEvents.h>
 #include <Core/EventSystem/EventDispatcher.h>
@@ -15,9 +17,13 @@
 namespace GLEngine::Editor {
 
 //=================================================================================
-C_Gizmo::C_Gizmo(const glm::vec3& position)
+C_Gizmo::C_Gizmo(const glm::vec3& position, const Core::I_Input& input)
 	: m_Position(position)
 	, m_ControllingGizmo(false)
+	, m_DiffSinceLastFrame(0.f)
+	, m_Input(input)
+	, m_LastMousePosition(0.f)
+	, m_MouseOffset(0.f)
 {
 }
 
@@ -34,17 +40,40 @@ void C_Gizmo::SetPosition(const glm::vec3& position)
 }
 
 //=================================================================================
-void C_Gizmo::OnUpdate(const Core::I_Input& input, const Renderer::I_CameraComponent& camera, C_MousePickingHelper& mouseHelper)
+void C_Gizmo::OnUpdate(const Renderer::I_CameraComponent& camera, C_MousePickingHelper& mouseHelper)
 {
 	// do not change mouse over when dragging gizmo
 	if (m_ControllingGizmo)
+	{
+		GLE_ASSERT(m_MouseOverDirection.has_value(), "When gizmo being controlled it needs to know which axe is being controlled");
+		// 1) Reproject to the world
+		// 2) Find the offset
+		// 3) Move the position
+		const auto ProjectedPosition = [&](const glm::vec2& mousePos, const glm::vec3& planeNormal) {
+			const auto					 ray = camera.GetRay(mousePos);
+			Physics::Primitives::S_Plane plane(planeNormal, m_Position);
+			const auto					 depth = plane.IntersectImpl(ray);
+			return ray.origin + ray.direction * depth;
+		};
+
+		const auto planeNormal = camera.GetPosition() - m_Position;
+
+		const auto originalPos = ProjectedPosition(m_LastMousePosition, planeNormal);
+		const auto newPos	   = ProjectedPosition(m_LastMousePosition + m_MouseOffset, planeNormal);
+		m_DiffSinceLastFrame   = newPos - originalPos;
+
+		m_DiffSinceLastFrame *= GetAxe(m_MouseOverDirection.value());
+
+		m_Position += m_DiffSinceLastFrame;
+
+		m_LastMousePosition = m_Input.GetClipSpaceMouseCoord();
+		m_MouseOffset		= {0.f, 0.f};
 		return;
+	}
 
-	const auto mosuePosition = input.GetClipSpaceMouseCoord();
-
-	mouseHelper.LineSegment(m_Position, m_Position + glm::vec3{1, 0, 0}, [&]() { m_MouseOverDirection = static_cast<E_Direction>(0); });
-	mouseHelper.LineSegment(m_Position, m_Position + glm::vec3{0, 1, 0}, [&]() { m_MouseOverDirection = static_cast<E_Direction>(1); });
-	mouseHelper.LineSegment(m_Position, m_Position + glm::vec3{0, 0, 1}, [&]() { m_MouseOverDirection = static_cast<E_Direction>(2); });
+	mouseHelper.LineSegment(m_Position, m_Position + GetAxe(E_Direction::Right), [&]() { m_MouseOverDirection = E_Direction::Right; });
+	mouseHelper.LineSegment(m_Position, m_Position + GetAxe(E_Direction::Up), [&]() { m_MouseOverDirection = E_Direction::Up; });
+	mouseHelper.LineSegment(m_Position, m_Position + GetAxe(E_Direction::Forward), [&]() { m_MouseOverDirection = E_Direction::Forward; });
 
 	m_MouseOverDirection.reset();
 }
@@ -79,7 +108,8 @@ bool C_Gizmo::OnMouseKeyPressed(Core::C_MouseButtonPressed& event)
 {
 	if (IsMouseOverGizmo())
 	{
-		m_ControllingGizmo = true;
+		m_ControllingGizmo	= true;
+		m_LastMousePosition = m_Input.GetClipSpaceMouseCoord();
 		return true;
 	}
 	return false;
@@ -108,10 +138,8 @@ bool C_Gizmo::OnMouseMoved(Core::C_MouseMoved& event)
 	if (m_ControllingGizmo == false)
 		return false;
 
-	//todo:
-	// 1) Reproject to the world
-	// 2) Find the offset
-	// 3) Move the position
+	m_MouseOffset += m_Input.GetClipSpaceMouseCoord() - m_LastMousePosition;
+
 	return true;
 }
 
@@ -119,6 +147,35 @@ bool C_Gizmo::OnMouseMoved(Core::C_MouseMoved& event)
 bool C_Gizmo::IsBeingControlled() const
 {
 	return m_ControllingGizmo;
+}
+
+//=================================================================================
+glm::vec3 C_Gizmo::GetPositionDiff() const
+{
+	if (IsBeingControlled())
+		return m_DiffSinceLastFrame;
+	return {0.f, 0.f, 0.f};
+}
+
+//=================================================================================
+glm::vec3 C_Gizmo::GetAxe(const E_Direction dir) const
+{
+	switch (dir)
+	{
+	case E_Direction::Right:
+		return {1, 0, 0};
+		break;
+	case E_Direction::Up:
+		return {0, 1, 0};
+		break;
+	case E_Direction::Forward:
+		return {0, 0, 1};
+		break;
+	default:
+		GLE_ASSERT(false, "This should not happen");
+		return {};
+		break;
+	}
 }
 
 } // namespace GLEngine::Editor
--- Editor/Editor/Gizmo.h
+++ Editor/Editor/Gizmo.h
@@ -20,16 +20,17 @@ class C_MousePickingHelper;
 
 class C_Gizmo : Core::I_EventReciever {
 public:
-	C_Gizmo(const glm::vec3& position);
+	C_Gizmo(const glm::vec3& position, const Core::I_Input& input);
 
 	[[nodiscard]] glm::vec3 GetPosition() const;
 	void					SetPosition(const glm::vec3& position);
 
-	void OnUpdate(const Core::I_Input& input, const Renderer::I_CameraComponent& camera, C_MousePickingHelper& mouseHelper);
+	void OnUpdate(const Renderer::I_CameraComponent& camera, C_MousePickingHelper& mouseHelper);
 	void Draw(Renderer::I_DebugDraw& dd) const;
 
-	[[nodiscard]] bool IsMouseOverGizmo() const;
-	[[nodiscard]] bool IsBeingControlled() const;
+	[[nodiscard]] glm::vec3 GetPositionDiff() const;
+	[[nodiscard]] bool		IsMouseOverGizmo() const;
+	[[nodiscard]] bool		IsBeingControlled() const;
 
 	void OnEvent(Core::I_Event& event) override;
 
@@ -45,8 +46,14 @@ private:
 		Up,
 		Forward
 	};
+	[[nodiscard]] glm::vec3 GetAxe(const E_Direction dir) const;
+
 	glm::vec3				   m_Position;
+	glm::vec3				   m_DiffSinceLastFrame;
 	std::optional<E_Direction> m_MouseOverDirection;
 	bool					   m_ControllingGizmo;
+	const Core::I_Input&	   m_Input;
+	glm::vec2				   m_LastMousePosition; //< In clip space
+	glm::vec2				   m_MouseOffset;		//< In clip space
 };
 } // namespace GLEngine::Editor
