6030da25b8dfb1a40de040fbacb247b6ecefcade
RohacekD <RohacekD@gmail.com>
2021-06-15 18:07:27 +0200

    Move animation timestamp into the animation module

---
+++ Animation/Animation/Timestamp.cpp
@@ -0,0 +1,50 @@
+#include <AnimationStdAfx.h>
+
+#include <Animation/Timestamp.h>
+
+namespace GLEngine::Animation {
+
+//=================================================================================
+// S_Timestamp
+//=================================================================================
+S_Timestamp::S_Timestamp(float timestamp)
+	: m_Timestamp(S_Timestamp::Clamp(timestamp))
+{
+}
+
+//=================================================================================
+S_Timestamp::S_Timestamp()
+	: m_Timestamp(0.f)
+{
+}
+
+//=================================================================================
+float S_Timestamp::Clamp(float f)
+{
+	return std::clamp(f, 0.0f, 1.0f);
+}
+
+//=================================================================================
+bool S_Timestamp::operator>=(const S_Timestamp& other) const
+{
+	return m_Timestamp >= other.m_Timestamp;
+}
+
+//=================================================================================
+bool S_Timestamp::operator>(const S_Timestamp& other) const
+{
+	return m_Timestamp > other.m_Timestamp;
+}
+
+//=================================================================================
+S_Timestamp S_Timestamp::operator-(const S_Timestamp& rhs) const
+{
+	return S_Timestamp(m_Timestamp - rhs.m_Timestamp);
+}
+
+//=================================================================================
+S_Timestamp S_Timestamp::operator+(const S_Timestamp& rhs) const
+{
+	return S_Timestamp(m_Timestamp + rhs.m_Timestamp);
+}
+}
---
+++ Animation/Animation/Timestamp.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Animation/AnimationApi.h>
+
+namespace GLEngine::Animation {
+
+struct ANIMATION_API_EXPORT S_Timestamp {
+	S_Timestamp();
+	S_Timestamp(float timestamp);
+
+	[[nodiscard]] bool		  operator>=(const S_Timestamp& other) const;
+	[[nodiscard]] bool		  operator>(const S_Timestamp& other) const;
+	[[nodiscard]] S_Timestamp operator-(const S_Timestamp& rhs) const;
+	[[nodiscard]] S_Timestamp operator+(const S_Timestamp& rhs) const;
+	[[nodiscard]] float		  GetValue() const { return m_Timestamp; }
+
+private:
+	static float Clamp(float f);
+	float		 m_Timestamp;
+};
+
+} // namespace GLEngine::Animation
