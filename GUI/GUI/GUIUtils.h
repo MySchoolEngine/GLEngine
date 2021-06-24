8897cd218a5218dc1eff950c5a375560f251f923
RohacekD <RohacekD@gmail.com>
2021-06-24 10:25:19 +0200

    Add ImGui utils for custom widgets

---
+++ GUI/GUI/GUIUtils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <GUI/GUIApi.h>
+
+struct ImVec2;
+struct ImDrawList;
+
+namespace GLEngine::GUI {
+enum class E_ButtonType
+{
+	Plus,
+	Minus,
+	Cross
+};
+// Returns true if mouse over button
+GUI_API_EXPORT [[nodiscard]] bool DrawSquareButton(ImDrawList* draw_list, const ImVec2 pos, const E_ButtonType type);
+}
+
