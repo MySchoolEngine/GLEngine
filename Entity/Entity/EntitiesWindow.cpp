d69fba03890897963392207e89a0b10289eabc73
RohacekD <RohacekD@gmail.com>
2022-02-11 16:40:33 +0100

    Better select/unselect entity in entities window

--- Entity/Entity/EntitiesWindow.cpp
+++ Entity/Entity/EntitiesWindow.cpp
@@ -32,10 +32,14 @@ void C_EntitiesWindow::Draw() const
 			Core::C_EntityEvent event(entity->GetID(), Core::C_EntityEvent::EntityEvent::Seleced);
 			//TODO: I need to sand this to the event tree root
 			entity->OnEvent(event);
-			m_SelectedEntity = entity->GetID();
+			if (m_SelectedEntity == entity->GetID())
+				m_SelectedEntity = GUID::INVALID_GUID;
+			else
+				m_SelectedEntity = entity->GetID();
 		}
 	}
 
+	// here should be way how to add components
 	auto entity = m_World->GetEntity(m_SelectedEntity);
 	if (entity)
 	{
