caf590efa0e39eb70bbf03eef75fe84ceeddd85f
RohacekD <RohacekD@gmail.com>
2022-02-12 17:24:43 +0100

    Improved layout for Entities window

--- GUI/GUI/GUIUtils.h
+++ GUI/GUI/GUIUtils.h
@@ -14,5 +14,6 @@ enum class E_ButtonType
 };
 // Returns true if mouse over button
 GUI_API_EXPORT [[nodiscard]] bool DrawSquareButton(ImDrawList* draw_list, const ImVec2 pos, const E_ButtonType type);
+GUI_API_EXPORT [[nodiscard]] float Splitter();
 }
 
