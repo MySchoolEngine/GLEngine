7de6fe57148ecd0536bb5573b4029ada3d62da15
RohacekD <RohacekD@gmail.com>
2022-04-10 23:43:19 +0200

    Builds fix

--- GLRenderer/GLRenderer/OGLDevice.h
+++ GLRenderer/GLRenderer/OGLDevice.h
@@ -7,13 +7,13 @@ namespace GLEngine::GLRenderer {
 class C_GLDevice : public Renderer::I_Device {
 public:
 	C_GLDevice();
-	virtual [[nodiscard]] bool AllocateTexture(Renderer::I_DeviceTexture& texture) override;
+	[[nodiscard]] virtual bool AllocateTexture(Renderer::I_DeviceTexture& texture) override;
 	virtual void			   DestroyTexture(Renderer::I_DeviceTexture& texture) override;
 
 	virtual std::size_t GetAllocatedMemory() const override;
 
 
-	virtual bool AllocateSampler(Renderer::I_TextureSampler2D& texture) override;
+	[[nodiscard]] virtual bool AllocateSampler(Renderer::I_TextureSampler2D& texture) override;
 	virtual void DestroySampler(Renderer::I_TextureSampler2D& texture) override;
 
 private:
