d76dcf2138d5e4b382dd8363f76db6dda701ca35
RohacekD <RohacekD@gmail.com>
2022-05-01 18:35:49 +0200

    Selected type GUI return type

--- GUI/GUI/Input/TypeSelector.h
+++ GUI/GUI/Input/TypeSelector.h
@@ -12,9 +12,23 @@ public:
 	C_TypeSelector(const std::string_view name, const std::string& baseTypeName);
 	void Draw() const override;
 
+	const std::string& GetSelectedTypeName() const { return m_Selected; }
+
 private:
 	std::string			m_BaseTypeName;
 	std::string			m_Name;
 	mutable std::string m_Selected;
+};
+
+template<class BaseType>
+class GUI_API_EXPORT C_CTTypeSelector : public C_TypeSelector {
+public:
+	C_CTTypeSelector(const std::string_view name)
+		: C_TypeSelector(name, rttr::type<BaseType>().get_name())
+	{}
+
+	[[nodiscard]] BaseType* GetInstnace() const {
+
+	}
 };
 } // namespace GLEngine::GUI::Input
