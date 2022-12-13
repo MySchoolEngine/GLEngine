45a384eec69e915293c2bfeedf789ce8d879cecb
RohacekD <RohacekD@gmail.com>
2022-12-13 21:14:06 +0100

    Crude direct lighting for skinned mesh

--- data/Shaders/animation/vertex.glsl
+++ data/Shaders/animation/vertex.glsl
@@ -3,9 +3,9 @@
 #define NUM_JOINTS	67
 #define MAX_WEIGHTS 3
 
-attribute vec4	vertex;
-attribute vec3	normal;
-attribute vec2	texCoord;
+layout(location = 0) in vec4 vertex;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec2 texCoord;
 attribute ivec3 jointIndices; // contains ids of joints affecting current vertex
 attribute vec3	weights;	  // weights should add to 1 togather
 
@@ -20,9 +20,9 @@ uniform mat4 transforms[NUM_JOINTS];
 //} jointTransforms;
 
 //=================================================================================
-out vec3 normalOUT;
-out vec2 texCoordOUT;
-out vec4 worldCoord;
+layout(location = 0) out vec3 normalOUT;
+layout(location = 1) out vec2 texCoordOUT;
+layout(location = 2) out vec4 worldCoord;
 
 
 //=================================================================================
