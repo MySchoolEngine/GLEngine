0ee4444401001fa03a00beed4abda20206358ba5
RohacekD <RohacekD@gmail.com>
2024-03-29 13:28:40 +0100

    Texture storage tests and documentation

--- Renderer/Renderer/Textures/TextureStorage.h
+++ Renderer/Renderer/Textures/TextureStorage.h
@@ -10,28 +10,71 @@ class RENDERER_API_EXPORT I_TextureViewStorage {
 public:
 	I_TextureViewStorage(std::size_t width, std::size_t height);
 	virtual ~I_TextureViewStorage();
-	[[nodiscard]] virtual float		GetF(std::size_t position) const	   = 0;
-	[[nodiscard]] virtual int		GetI(std::size_t position) const	   = 0;
+	/**
+	 * @param	position	element position in row by row and element after element
+	 * @returns	float		value on given position
+	 */
+	[[nodiscard]] virtual float GetF(std::size_t position) const = 0;
+	/**
+	 * @param	position	element position in row by row and element after element
+	 * @returns	int			value on given position
+	 */
+	[[nodiscard]] virtual int GetI(std::size_t position) const = 0;
+	/**
+	 * @param	pixelIndex	element position in row by row indexed by whole pixels. @todo should be swizzled
+	 * @returns	float		value on given position. Unused elements are not defined
+	 */
 	[[nodiscard]] virtual glm::vec4 GetPixel(std::size_t pixelIndex) const = 0;
 
-	void		 Set(double value, std::size_t position);
-	void		 Set(int value, std::size_t position);
-	virtual void SetPixel(const glm::vec4& value, std::size_t position) = 0; //< position denotes pixels index
-	virtual void SetAll(const glm::vec4& value)							= 0;
+	/**
+	 * @param	value		value to be stored
+	 * @param	position	element position in row by row and channel after channel.
+	 */
+	void Set(double value, std::size_t position);
+	/**
+	 * @param	value		value to be stored
+	 * @param	position	element position in row by row and channel after channel.
+	 */
+	void Set(int value, std::size_t position);
+	/**
+	 * @param	value		value to be saved. In RGBA format. This will be internally
+	 *						swizzled to correct channel order
+	 * @param	position	pixel index in row by row linear array
+	 */
+	virtual void SetPixel(const glm::vec4& value, std::size_t position) = 0;
+	/**
+	 * Sets same value to all pixels. Basically clear colour buffer
+	 * @param	value		value to be stored
+	 */
+	virtual void SetAll(const glm::vec4& value) = 0;
 
 	[[nodiscard]] virtual const void* GetData() const							 = 0;
 	[[nodiscard]] virtual void*		  GetData()									 = 0;
 	virtual const void				  SetData(const void* data, std::size_t len) = 0;
 
+	/**
+	 * @returns	uint8_t	Number of channels
+	 */
 	[[nodiscard]] virtual std::uint8_t GetNumElements() const							= 0;
 	[[nodiscard]] virtual std::uint8_t GetChannelOffset(E_TextureChannel element) const = 0;
 
 	[[nodiscard]] glm::uvec2 GetDimensions() const;
 
-	// defines how channels are laid out in memory
+	/************************************************************************/
+	/* Swizzle                                                              */
+	/************************************************************************/
+	/**
+	 * @returns	T_Channels	Channel ordering in memory.
+	 */
 	[[nodiscard]] T_Channels GetChannels() const { return m_Channels; }
-	void					 SetChannels(T_Channels swizzle) { m_Channels = swizzle; }
-	[[nodiscard]] bool		 IsSwizzled() const;
+	/**
+	 * Sets the ordering of channels in memory. Each colour channel should have unique index.
+	 */
+	void SetChannels(T_Channels swizzle);
+	/**
+	 * @returns	true	if ordered RGBA with trailing channels possible missing. false otherwise
+	 */
+	[[nodiscard]] bool IsSwizzled() const;
 
 	[[nodiscard]] virtual E_TextureTypes GetStorageType() const = 0;
 
@@ -55,8 +98,11 @@ public:
 	[[nodiscard]] virtual int		GetI(std::size_t position) const override;
 	[[nodiscard]] virtual glm::vec4 GetPixel(std::size_t pixelIndex) const override;
 
-	virtual void SetPixel(const glm::vec4& value, std::size_t position) override; //< position denotes pixels index
+	virtual void SetPixel(const glm::vec4& value, std::size_t position) override;
 
+	/**
+	 * @return	Number of channels per pixel
+	 */
 	[[nodiscard]] virtual std::uint8_t GetNumElements() const override;
 
 	[[nodiscard]] virtual const void* GetData() const override;
