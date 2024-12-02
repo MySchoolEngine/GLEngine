43865d9af2790960c1cdcee531e6f29b8ab5987a
RohacekD <RohacekD@gmail.com>
2024-12-02 23:27:12 +0100

    Paths for resources

--- Core/Core/Filesystem/Paths.h
+++ Core/Core/Filesystem/Paths.h
@@ -3,4 +3,7 @@
 namespace GLEngine::Core::Filesystem {
 inline const static std::filesystem::path entityFile = ".ent";
 inline const static std::filesystem::path entityFolder = "./Entities";
+inline const static std::filesystem::path imageFolder = "./Images";
+inline const static std::filesystem::path modelFolder = "./Models";
+inline const static std::filesystem::path levelsFolder = "./Levels";
 }
