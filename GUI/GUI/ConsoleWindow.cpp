86b2273052033d74630128883be38182dabc82f1
RohacekD <RohacekD@gmail.com>
2024-12-07 23:56:29 +0100

    Fixes #126
    Openfile On click

--- GUI/GUI/ConsoleWindow.cpp
+++ GUI/GUI/ConsoleWindow.cpp
@@ -3,18 +3,80 @@
 #include <GUI/ConsoleWindow.h>
 #include <GUI/GUIPart.h>
 
+#include <GUI/ReflectionGUI.h>
+
 #include <Utils/Logging/Filter.h>
+#include <Utils/Reflection/Metadata.h>
+
+#if _WIN32
+#include <shellapi.h>
+#endif
 
 #include <fmt/format.h>
 
-namespace GLEngine::GUI {
+// clang-format off
+RTTR_REGISTRATION
+{
+	using namespace GLEngine::GUI;
+	using namespace Utils::Reflection;
 
-std::array<ImVec4, 4> C_ConsoleWindow::s_LevelColors = {ImVec4(1, 0, 0, 1), ImVec4(1, 0.8f, 0, 1), ImVec4(1, 1, 1, 1), ImVec4(0.7f, 0.7f, 0.7f, 1.0)};
+	rttr::registration::class_<C_ConsoleWindow>("C_ConsoleWindow")
+		.property("Level", &C_ConsoleWindow::m_Level)
+		(
+			rttr::policy::prop::as_reference_wrapper,
+			RegisterMetaclass<MetaGUI::EnumSelectOptional>(),
+			RegisterMetamember<UI::EnumSelectOptional::Name>("Level:"),
+			RegisterMetamember<UI::EnumSelectOptional::OptionalName>("All"),
+			RegisterMetamember<SerializationCls::NoSerialize>(true)
+		)
+		.property("Context", &C_ConsoleWindow::m_Context)
+		(
+			rttr::policy::prop::as_reference_wrapper,
+			RegisterMetaclass<MetaGUI::EnumSelectOptional>(),
+			RegisterMetamember<UI::EnumSelectOptional::Name>("Context:"),
+			RegisterMetamember<UI::EnumSelectOptional::OptionalName>("All"),
+			RegisterMetamember<SerializationCls::NoSerialize>(true)
+		);
+
+	rttr::registration::class_<std::optional<Utils::Logging::E_Level>>("std::optional<Utils::Logging::E_Level>");
+	rttr::registration::class_<std::optional<Utils::Logging::E_Level>>("std::optional<Utils::Logging::E_Context>");
+}
+// clang-format on
+
+namespace rttr
+{
+template<typename T>
+struct wrapper_mapper<std::optional<T>>
+{
+	using wrapped_type = const T*;
+	using type = std::optional<T>;
+
+	static RTTR_INLINE const wrapped_type get(const type& obj)
+	{
+		if (obj.has_value())
+			return &obj.value();
+		return nullptr;
+	}
+
+	static RTTR_INLINE type create(const wrapped_type& t)
+	{
+		if (!t)
+			return std::nullopt;
+		return type(*t);
+	}
+};
+}
+
+namespace GLEngine::GUI
+{
+
+std::array<ImVec4, 4> C_ConsoleWindow::s_LevelColors = { ImVec4(1, 0, 0, 1), ImVec4(1, 0.8f, 0, 1), ImVec4(1, 1, 1, 1), ImVec4(0.7f, 0.7f, 0.7f, 1.0) };
 
 //=================================================================================
 C_ConsoleWindow::C_ConsoleWindow(GUID guid)
 	: C_Window(guid, "Console")
 	, m_LastLogIndex(m_Logs.size() - 1)
+	, m_Level(Utils::Logging::E_Level::Info)
 {
 	Utils::Logging::C_LoggingSystem::Instance().AddLogger(this);
 }
@@ -31,85 +93,44 @@ void C_ConsoleWindow::Log(Utils::Logging::E_Level level, Utils::Logging::E_Conte
 {
 	m_LastLogIndex++;
 	m_LastLogIndex %= m_Logs.size();
-	m_Logs[m_LastLogIndex] = Utils::Logging::S_Data{text, file, line, level, context};
+	m_Logs[m_LastLogIndex] = Utils::Logging::S_Data{ text, file, line, level, context };
 }
 
 //=================================================================================
 void C_ConsoleWindow::Draw() const
 {
 	::ImGui::Begin(m_Name.c_str(), &m_IsVisible);
-	static const char* levels[]			 = {"Error", "Warning", "Info", "Debug", "None"};
-	static const char* contexts[]		 = {"Core", "Render", "Entity", "None"};
-	static const char* current_level	 = levels[4];
-	static const char* current_context	 = contexts[3];
-	static int		   selectedLevelId	 = 4;
-	static int		   selectedContextId = 3;
-	using levelUnderlying				 = std::underlying_type_t<Utils::Logging::E_Level>;
-	using contextUnderlying				 = std::underlying_type_t<Utils::Logging::E_Context>;
-
-	static char m_SpawningName[255] = {0};
+	rttr::instance obj(*this);
+	if (GUI::DrawAllPropertyGUI(obj).empty() == false)
+	{
+		//m_Changed = true;
+	}
+	using levelUnderlying = std::underlying_type_t<Utils::Logging::E_Level>;
+	static char filteredText[255] = { 0 };
 
 	static std::unique_ptr<Utils::Logging::C_Filter> filter = std::make_unique<Utils::Logging::C_PassAllFilter>();
 
-	if (::ImGui::BeginCombo("##level", current_level))
-	{
-		for (int n = 0; n < sizeof(levels) / sizeof(char*); n++)
-		{
-			bool is_selected = (current_level == levels[n]);
-			if (::ImGui::Selectable(levels[n], is_selected))
-			{
-				current_level	= levels[n];
-				selectedLevelId = n;
-			}
-			if (is_selected)
-			{
-				::ImGui::SetItemDefaultFocus();
-			}
-		}
-		::ImGui::EndCombo();
-	}
-
-
-	if (::ImGui::BeginCombo("##context", current_context))
-	{
-		for (int n = 0; n < sizeof(contexts) / sizeof(char*); n++)
-		{
-			bool is_selected = (current_context == contexts[n]);
-			if (::ImGui::Selectable(contexts[n], is_selected))
-			{
-				current_context	  = contexts[n];
-				selectedContextId = n;
-			}
-			if (is_selected)
-			{
-				::ImGui::SetItemDefaultFocus();
-			}
-		}
-		::ImGui::EndCombo();
-	}
-
-
-	::ImGui::InputText("Filtered text", m_SpawningName, 255);
+	::ImGui::InputText("Filtered text", filteredText, 255);
 
 	Utils::Logging::C_Filter* innerFilter(nullptr);
 
-	if (selectedContextId != 3)
+	if (m_Context.has_value())
 	{
 		using namespace Utils::Logging;
-		innerFilter = new C_NegationFilter(new C_ContextFilter(static_cast<E_Context>(selectedContextId)));
+		innerFilter = new C_NegationFilter(new C_ContextFilter(static_cast<E_Context>(m_Context.value())));
 	}
 
-	if (strlen(m_SpawningName) != 0)
+	if (strlen(filteredText) != 0)
 	{
 		using namespace Utils::Logging;
-		innerFilter = new C_TextFilter(m_SpawningName, innerFilter);
+		innerFilter = new C_TextFilter(filteredText, innerFilter);
 	}
 
 
-	if (selectedLevelId != 4)
+	if (m_Level.has_value())
 	{
 		using namespace Utils::Logging;
-		filter = std::make_unique<C_NegationFilter>(new C_LevelFilter(static_cast<E_Level>(selectedLevelId), innerFilter));
+		filter = std::make_unique<C_NegationFilter>(new C_LevelFilter(static_cast<E_Level>(m_Level.value()), innerFilter));
 	}
 	else
 	{
@@ -121,18 +142,28 @@ void C_ConsoleWindow::Draw() const
 	}
 
 	::ImGui::BeginChild("Output");
-	for (std::size_t i = ((m_LastLogIndex + 1) % m_Logs.size()); i != m_LastLogIndex; i = ((i + 1) % m_Logs.size()))
+	std::size_t i = ((m_LastLogIndex + 1) % m_Logs.size());
+	do
 	{
 		auto& data = m_Logs[i];
 
-		if (data.m_Text.empty())
+		if (!data.m_Text.empty() && filter->Filter(data))
 		{
-			continue;
+			::ImGui::TextColored(s_LevelColors[static_cast<levelUnderlying>(data.m_Level)], data.m_Text.c_str());
+			if (::ImGui::IsItemHovered(ImGuiHoveredFlags_AllowWhenDisabled))
+			{
+				ImGui::SetMouseCursor(ImGuiMouseCursor_::ImGuiMouseCursor_Hand);
+				const auto linkText = data.m_File + ":" + std::to_string(data.m_Line);
+				::ImGui::SetTooltip(linkText.c_str());
+#if _WIN32
+				if(::ImGui::IsItemClicked())
+					ShellExecute(0, 0, std::wstring(data.m_File.begin(), data.m_File.end()).c_str(), 0, 0, SW_SHOW);
+#endif
+			}
+
 		}
-		if (!filter->Filter(data))
-			continue;
-		::ImGui::TextColored(s_LevelColors[static_cast<levelUnderlying>(data.m_Level)], data.m_Text.c_str());
-	}
+		i = ((i + 1) % m_Logs.size());
+	} while (i != m_LastLogIndex + 1);
 	::ImGui::SetScrollHereY(1.0f);
 	::ImGui::EndChild();
 	::ImGui::End();
--- GUI/GUI/ConsoleWindow.h
+++ GUI/GUI/ConsoleWindow.h
@@ -5,8 +5,13 @@
 #include <Utils/Logging/Data.h>
 #include <Utils/Logging/ILogger.h>
 
-namespace GLEngine::GUI {
-class GUI_API_EXPORT C_ConsoleWindow : public C_Window, public Utils::Logging::I_Logger {
+#include <rttr/registration.h>
+#include <rttr/registration_friend.h>
+
+namespace GLEngine::GUI
+{
+class GUI_API_EXPORT C_ConsoleWindow : public C_Window, public Utils::Logging::I_Logger
+{
 public:
 	explicit C_ConsoleWindow(GUID guid);
 	virtual ~C_ConsoleWindow();
@@ -18,8 +23,12 @@ private:
 	//@todo replace with stack vector once we have 'em
 	std::array<Utils::Logging::S_Data, 1000> m_Logs;
 	std::size_t								 m_LastLogIndex;
+	std::optional<Utils::Logging::E_Level>	 m_Level;
+	std::optional<Utils::Logging::E_Context> m_Context;
 
 	static std::array<ImVec4, 4> s_LevelColors;
+	RTTR_ENABLE();
+	RTTR_REGISTRATION_FRIEND;
 };
 
 } // namespace GLEngine::GUI
