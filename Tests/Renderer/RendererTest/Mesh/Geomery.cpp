44eaea1b67bf4c67b2bf24b42d15d9f956f26e5f
RohacekD <RohacekD@gmail.com>
2024-12-30 10:10:55 +0100

    Fix generated plane size

--- Tests/Renderer/RendererTest/Mesh/Geomery.cpp
+++ Tests/Renderer/RendererTest/Mesh/Geomery.cpp
@@ -8,13 +8,9 @@
 
 namespace GLEngine::Renderer::MeshData {
 
-class GeometryPlane : public ::testing::TestWithParam<float> {
-	
-};
+class GeometryPlane : public ::testing::TestWithParam<float> {};
 
-INSTANTIATE_TEST_SUITE_P(PlaneSize,
-	GeometryPlane,
-	::testing::Values(1.f, 2.f, 10.f));
+INSTANTIATE_TEST_SUITE_P(PlaneSize, GeometryPlane, ::testing::Values(.5f, 1.f, 2.f, 10.f));
 
 TEST_P(GeometryPlane, CreatePlane)
 {
@@ -22,10 +18,16 @@ TEST_P(GeometryPlane, CreatePlane)
 	EXPECT_EQ(plane.vertices.size(), 6);
 	EXPECT_EQ(plane.normals.size(), 6);
 	EXPECT_EQ(plane.texcoords.size(), 6);
-	EXPECT_TRUE(Utils::contains(plane.texcoords, glm::vec2{ 1.f, 1.f }));
-	EXPECT_TRUE(Utils::contains(plane.texcoords, glm::vec2{ 0.f, 1.f }));
-	EXPECT_TRUE(Utils::contains(plane.texcoords, glm::vec2{ 1.f, 0.f }));
-	EXPECT_TRUE(Utils::contains(plane.texcoords, glm::vec2{ 0.f, 0.f }));
+	EXPECT_TRUE(Utils::contains(plane.texcoords, glm::vec2{1.f, 1.f}));
+	EXPECT_TRUE(Utils::contains(plane.texcoords, glm::vec2{0.f, 1.f}));
+	EXPECT_TRUE(Utils::contains(plane.texcoords, glm::vec2{1.f, 0.f}));
+	EXPECT_TRUE(Utils::contains(plane.texcoords, glm::vec2{0.f, 0.f}));
+
+	const auto expectedCorners = GetParam() / 2.f;
+	EXPECT_TRUE(Utils::contains(plane.vertices, glm::vec3(-expectedCorners, 0.f, -expectedCorners)));
+	EXPECT_TRUE(Utils::contains(plane.vertices, glm::vec3(-expectedCorners, 0.f, expectedCorners)));
+	EXPECT_TRUE(Utils::contains(plane.vertices, glm::vec3(expectedCorners, 0.f, -expectedCorners)));
+	EXPECT_TRUE(Utils::contains(plane.vertices, glm::vec3(expectedCorners, 0.f, expectedCorners)));
 	for (const auto& texCoord : plane.texcoords)
 	{
 		EXPECT_THAT(texCoord.x, testing::AllOf(testing::Ge(0.f), testing::Le(1.f)));
