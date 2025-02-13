dedd0f095dd72c241f988c2295b63edbe49aef3f
RohacekD <RohacekD@gmail.com>
2025-02-13 22:12:41 +0100

    Compilation fix

--- Renderer/Renderer/Render/PhysicsRenderer.h
+++ Renderer/Renderer/Render/PhysicsRenderer.h
@@ -12,6 +12,5 @@ class RENDERER_API_EXPORT C_PhysicsRenderer {
 public:
 	static void Render(C_TextureView& view, const Physics::Primitives::Plane2D& plane, const Colours::T_Colour& colour);
 	static void Render(C_TextureView& view, const Core::S_Rect& box2D, const Colours::T_Colour& colour);
-	static void Render(C_TextureView& view, const Physics::Primitives::S_LineSegment& plane, const Colours::T_Colour& colour);
 };
 } // namespace GLEngine::Renderer
