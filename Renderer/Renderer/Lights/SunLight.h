ff62c2a34bb4b9e8decb4ddc1abff6b2b2223950
RohacekD <RohacekD@gmail.com>
2025-02-16 14:34:49 +0100

    Sun light default colours
    Formatting

--- Renderer/Renderer/Lights/SunLight.h
+++ Renderer/Renderer/Lights/SunLight.h
@@ -12,7 +12,7 @@ class RENDERER_API_EXPORT C_SunLight : public I_Light {
 public:
 	C_SunLight(const std::shared_ptr<Entity::I_Entity>& owner);
 	C_SunLight();
-	virtual ~C_SunLight();
+	~C_SunLight() override;
 
 	[[nodiscard]] glm::vec3			GetSunDirection() const;
 	[[nodiscard]] Colours::T_Colour GetSunColor() const;
@@ -22,13 +22,13 @@ public:
 	[[nodiscard]] Physics::Primitives::C_Frustum GetShadingFrustum() const override;
 	[[nodiscard]] bool							 HasDebugDrawGUI() const override;
 	[[nodiscard]] std::string_view				 GetDebugComponentName() const override;
-	virtual void								 DebugDrawGUI() override;
+	void										 DebugDrawGUI() override;
 
 	void									  DebugDraw(I_DebugDraw* dd) const;
 	[[nodiscard]] Physics::Primitives::S_AABB GetAABB() const override;
 
-	RTTR_ENABLE(I_Light);
-	RTTR_REGISTRATION_FRIEND;
+	RTTR_ENABLE(I_Light)
+	RTTR_REGISTRATION_FRIEND
 
 private:
 	float			  m_SunX;
