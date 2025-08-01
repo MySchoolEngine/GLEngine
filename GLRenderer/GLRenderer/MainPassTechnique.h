64f732172bb38e496bf1d4d0641238b840c1dd2a
RohacekD <RohacekD@gmail.com>
2025-08-02 00:31:55 +0200

    DI DebugRenderer and use new handle renderer

--- GLRenderer/GLRenderer/MainPassTechnique.h
+++ GLRenderer/GLRenderer/MainPassTechnique.h
@@ -2,6 +2,8 @@
 
 #include <Renderer/Resources/RenderResourceHandle.h>
 
+#include "Renderer/Renderer3D.h"
+
 namespace GLEngine::Entity {
 class C_EntityManager;
 }
@@ -9,7 +11,7 @@ class C_EntityManager;
 namespace GLEngine::Renderer {
 class ResourceManager;
 class I_CameraComponent;
-}
+} // namespace GLEngine::Renderer
 
 namespace GLEngine::GLRenderer {
 
@@ -26,7 +28,12 @@ class C_MaterialsBuffer;
 class C_MainPassTechnique {
 public:
 	explicit C_MainPassTechnique(Renderer::ResourceManager& resourceManager);
-	void Render(const Entity::C_EntityManager& world, std::shared_ptr<Renderer::I_CameraComponent> camera, unsigned int width, unsigned int height);
+	void Render(const Entity::C_EntityManager&				 world,
+				std::shared_ptr<Renderer::I_CameraComponent> camera,
+				unsigned int								 width,
+				unsigned int								 height,
+				Renderer::I_DebugDraw&						 dd,
+				Renderer::I_RenderInterface3D& renderInterface3D);
 	void SetSunShadowMap(Renderer::Handle<Renderer::Texture> sunShadowMapHandle);
 	void SetSunViewProjection(glm::mat4 viewProjection);
 
@@ -36,6 +43,8 @@ private:
 	Renderer::Handle<Renderer::Texture>					  m_SunShadowMap;
 	glm::mat4											  m_SunViewProjection;
 	std::shared_ptr<Material::C_MaterialsBuffer>		  m_MaterialsUBO;
+
+	Renderer::Renderer3D m_3DRenderer;
 };
 
 } // namespace GLEngine::GLRenderer
