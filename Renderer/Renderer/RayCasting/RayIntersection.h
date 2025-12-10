587f32e94340ed5465b084db6157afc61f0c41e3
RohacekD <RohacekD@gmail.com>
2025-12-10 22:13:23 +0100

    Improvements in RayIntersection

--- Renderer/Renderer/RayCasting/RayIntersection.h
+++ Renderer/Renderer/RayCasting/RayIntersection.h
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <Renderer/RendererApi.h>
-
 #include <Renderer/RayCasting/Frame.h>
 #include <Renderer/RayCasting/Material/MaterialInterface.h>
+#include <Renderer/RendererApi.h>
 
 #include <Physics/Primitives/Ray.h>
 
@@ -14,24 +13,39 @@ class I_RayLight;
 class RENDERER_API_EXPORT C_RayIntersection {
 public:
 	C_RayIntersection(); //= default;
-	C_RayIntersection(S_Frame&& frame, glm::vec3&& point, Physics::Primitives::S_Ray&& ray);
+	C_RayIntersection(const S_Frame& frame, const glm::vec3& point, const Physics::Primitives::S_Ray& ray);
 	~C_RayIntersection(); //= default;
+
+	// Explicitly default copy/move operations (or delete if not needed)
+	C_RayIntersection(const C_RayIntersection&)		= default;
+	C_RayIntersection(C_RayIntersection&&) noexcept = default;
+	C_RayIntersection& operator=(const C_RayIntersection&) = default;
+	C_RayIntersection& operator=(C_RayIntersection&&) noexcept = default;
 
 	[[nodiscard]] const S_Frame&					GetFrame() const;
 	[[nodiscard]] const Physics::Primitives::S_Ray& GetRay() const;
 	[[nodiscard]] const glm::vec3&					GetIntersectionPoint() const;
-	[[nodiscard]] float								GetRayLength() const;
-	void											SetRayLength(float t) { m_RayLength = t; }
+	/**
+	 * @brief
+	 * @return Length from the \var m_Ray.origin to intersection, infinity if ray missed
+	 */
+	[[nodiscard]] float GetRayLength() const;
+	void				SetRayLength(float t) { m_RayLength = t; }
 
+	/**
+	 * @brief Do not transfer the ownership
+	 * @param material 
+	 */
 	void									 SetMaterial(const I_MaterialInterface* material);
 	[[nodiscard]] const I_MaterialInterface* GetMaterial() const;
 
-	[[nodiscard]] bool											IsLight() const;
-	void														SetLight(const std::shared_ptr<RayTracing::I_RayLight>& light);
-	[[nodiscard]] const std::shared_ptr<RayTracing::I_RayLight> GetLight() const;
+	[[nodiscard]] bool											 IsLight() const;
+	void														 SetLight(const std::shared_ptr<RayTracing::I_RayLight>& light);
+	void														 SetLight(std::shared_ptr<RayTracing::I_RayLight>&& light);
+	[[nodiscard]] const std::shared_ptr<RayTracing::I_RayLight>& GetLight() const;
 
-	glm::vec2 GetUV() const { return m_UV; }
-	void	  SetUV(const glm::vec2& UV) { m_UV = UV; }
+	[[nodiscard]] glm::vec2 GetUV() const { return m_UV; }
+	void					SetUV(const glm::vec2& UV) { m_UV = UV; }
 
 	void TransformRayAndPoint(const glm::mat4& mat);
 
@@ -39,9 +53,9 @@ private:
 	S_Frame									m_Frame;
 	glm::vec3								m_Point;
 	Physics::Primitives::S_Ray				m_Ray;
-	glm::vec2								m_UV = {0.f, 0.f};
-	float									m_RayLength;
-	const I_MaterialInterface*				m_Material = nullptr; // not owning
-	std::shared_ptr<RayTracing::I_RayLight> m_Light	   = nullptr;
+	glm::vec2								m_UV		= {0.f, 0.f};
+	float									m_RayLength = std::numeric_limits<float>::infinity();
+	const I_MaterialInterface*				m_Material	= nullptr; // not owning
+	std::shared_ptr<RayTracing::I_RayLight> m_Light		= nullptr;
 };
 } // namespace GLEngine::Renderer
