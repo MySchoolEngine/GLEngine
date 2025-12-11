895505848b6587f78fa58bf1ad30e59a7ce9d9a3
RohacekD <RohacekD@gmail.com>
2025-12-11 21:47:25 +0100

    Allow for modules to be compiled statically for tests
    Compilation fix

--- Physics/Physics/Primitives/Triangle.h
+++ Physics/Physics/Primitives/Triangle.h
@@ -8,11 +8,35 @@
 
 namespace GLEngine::Physics::Primitives {
 
-struct S_Triangle final {
+struct S_Triangle final : public T_Intersectable<S_Triangle> {
 	enum class CreateError : std::uint8_t
 	{
 		CollinearPoints,
 	};
+
+	S_Triangle(const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2)
+		: m_p({p0, p1, p2})
+	{
+		CollinearPoints,
+	};
+
+	[[nodiscard]] static std::expected<S_Triangle, CreateError> Create(const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2)
+	{
+		S_Triangle ret;
+		ret.m_p			  = {p0, p1, p2};
+		const auto normal = glm::cross(p1 - p0, p2 - p0);
+		const auto length = glm::length(normal);
+		ret.m_Area		  = length / 2.f;
+
+		// Check for degenerate triangle (collinear points)
+		constexpr float EPSILON = 1e-8f;
+		if (length <= EPSILON)
+		{
+			return std::unexpected(CreateError::CollinearPoints);
+		}
+		ret.m_Normal = glm::normalize(normal);
+		return ret;
+	}
 
 	[[nodiscard]] static std::expected<S_Triangle, CreateError> Create(const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2)
 	{
