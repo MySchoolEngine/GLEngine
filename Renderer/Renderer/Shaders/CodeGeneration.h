20ad9c2a9f7507ad92fce9b2f8cd2750bda37d17
RohacekD <RohacekD@gmail.com>
2025-12-26 03:24:32 +0100

    Remove more warnings

--- Renderer/Renderer/Shaders/CodeGeneration.h
+++ Renderer/Renderer/Shaders/CodeGeneration.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <Renderer/RendererApi.h>
+
 #include <string>
 
 namespace GLEngine::Renderer::Shaders {
 
-class I_CodeProvider {
+class RENDERER_API_EXPORT I_CodeProvider {
 public:
 	virtual ~I_CodeProvider() = default;
 	[[nodiscard]] virtual std::string GetStructCode(const std::string& name) const = 0;
