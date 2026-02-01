1100a0d30b3e442650f801c833778faf97077e60
RohacekD <RohacekD@gmail.com>
2026-02-02 00:07:03 +0100

    Move the component to the handles #278

--- Renderer/Renderer/Mesh/GeomComponent.h
+++ Renderer/Renderer/Mesh/GeomComponent.h
@@ -3,30 +3,47 @@
 #include <Renderer/IRenderableComponent.h>
 #include <Renderer/Materials/Material.h>
 #include <Renderer/Mesh/Scene.h>
-#include <Renderer/RendererApi.h>
-
-#include <Entity/IComponent.h>
 
 #include <Utils/Parsing/MaterialParser.h>
 
 namespace GLEngine::Renderer {
+class Renderer3D;
 
 class C_Material;
 
-class RENDERER_API_EXPORT C_GeomComponent : public I_RenderableComponent {
+class C_GeomComponent : public I_RenderableComponent {
 public:
+	void Update() override;
+	C_GeomComponent();
 	C_GeomComponent(const std::shared_ptr<Entity::I_Entity>& owner);
-	virtual ~C_GeomComponent();
-	virtual void SetupGeometry(const MeshData::Mesh& mesh)				   = 0;
-	virtual void SetupMaterial(const ::Utils::Parsing::MaterialData& data) = 0;
+	~C_GeomComponent() override;
+	void SetupGeometry(const MeshData::Mesh& mesh);
+	void Render(Renderer3D& renderer) const;
+	void PerformDraw() const override {}
+	Physics::Primitives::S_AABB GetAABB() const override;
 
+	void			   AfterDeserialize(Utils::C_XMLDeserializer::DeserializeCtx& ctx);
 	void			   DebugDrawGUI() override;
 	[[nodiscard]] bool HasDebugDrawGUI() const override;
 
 	std::string_view GetDebugComponentName() const override;
 
+	RTTR_ENABLE(Renderer::I_RenderableComponent)
 protected:
+	struct MeshContainer {
+		Handle<Buffer> m_PositionsHandle;
+		Handle<Buffer> m_NormalsHandle;
+		Handle<Buffer> m_TexCoordsHandle;
+		Handle<Buffer> m_TangentHandle;
+		Handle<Buffer> m_BitangentHandle;
+		uint32_t	   m_NumPrimitives;
+	};
+	MeshContainer				m_Mesh;
+	Handle<Pipeline>			m_Pipeline;
 	std::shared_ptr<C_Material> m_Material;
+
+
+	RTTR_REGISTRATION_FRIEND
 };
 
 } // namespace GLEngine::Renderer
