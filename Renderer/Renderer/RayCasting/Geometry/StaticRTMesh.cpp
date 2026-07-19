27eb882a30a5979483cd7fd182833df11c868117
RohacekD <RohacekD@gmail.com>
2026-07-19 22:48:02 +0200

    Initial implementation for RTTrimesh models

---
+++ Renderer/Renderer/RayCasting/Geometry/StaticRTMesh.cpp
@@ -0,0 +1,64 @@
+﻿#include <RendererStdafx.h>
+
+#include <Renderer/RayCasting/Geometry/StaticRTMesh.h>
+
+
+//		.property("Transform", &C_Trimesh::m_Transform)(rttr::policy::prop::as_reference_wrapper, REGISTER_DEFAULT_VALUE(glm::mat4(1.f)))
+namespace GLEngine::Renderer {
+
+//=================================================================================
+C_StaticRTMesh::C_StaticRTMesh(const Core::ResourceHandle<C_TrimeshModel>& model)
+	: m_TrimeshModel(model)
+	, m_AABB(model.GetResource().GetAABB())
+{
+	GLE_ASSERT(model.IsReady(), "Only loaded C_TrimeshModel can be used to construct C_StaticRTMesh");
+}
+
+//=================================================================================
+C_StaticRTMesh::~C_StaticRTMesh() = default;
+
+//=================================================================================
+bool C_StaticRTMesh::Intersect(const Physics::Primitives::S_Ray& ray, C_RayIntersection& intersection, const float tMax) const
+{
+	const auto	rayTransformed = ray.GetTransformedRay(m_InvTransform);
+	const auto& trimeshes	   = m_TrimeshModel.GetResource().GetTrimeshes();
+	C_RayIntersection bestIntersection;
+	for (const auto& trimesh : trimeshes)
+	{
+		C_RayIntersection intersectionCandidate;
+		if (trimesh.Intersect(rayTransformed, intersectionCandidate, bestIntersection.GetRayLength()))
+		{
+			// because we limit via bestIntersection.GetRayLength() this have to be better hit
+			bestIntersection = intersectionCandidate;
+		}
+	}
+	if (std::isinf(bestIntersection.GetRayLength()))
+		return false;
+	intersection = bestIntersection;
+	// material
+	bestIntersection.SetMaterial(&GetMaterial());
+	// alpha mask
+	if (m_AlphaMask.IsReady())
+	{
+		intersection.SetAlphaMask(C_TextureView(const_cast<I_TextureViewStorage*>(&m_AlphaMask.GetResource().GetStorage())));
+	}
+	// todo normal correction
+
+	return false;
+}
+
+//=================================================================================
+float C_StaticRTMesh::Area() const
+{
+	return 0.f;
+}
+
+//=================================================================================
+void C_StaticRTMesh::SetTransformation(const glm::mat4& mat)
+{
+	auto invMat	   = glm::inverse(mat);
+	m_AABB		   = m_AABB.getTransformedAABB(mat);
+	m_Transform	   = mat;
+	m_InvTransform = invMat;
+}
+} // namespace GLEngine::Renderer
---
+++ Renderer/Renderer/RayCasting/Geometry/StaticRTMesh.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <Renderer/RayCasting/Geometry/SceneGeometry.h>
+#include <Renderer/RayCasting/Geometry/TrimeshModel.h>
+
+
+namespace GLEngine::Renderer {
+class C_TrimeshModel;
+class C_StaticRTMesh : public I_RayGeometryObject {
+public:
+	C_StaticRTMesh(const Core::ResourceHandle<C_TrimeshModel>& model);
+	~C_StaticRTMesh() override;
+	[[nodiscard]] bool Intersect(const Physics::Primitives::S_Ray& ray, C_RayIntersection& intersection, const float tMax) const override;
+
+	[[nodiscard]] float Area() const override;
+	void				SetTransformation(const glm::mat4& mat);
+
+private:
+	glm::mat4							 m_Transform	= glm::mat4(1.f);
+	glm::mat4							 m_InvTransform = glm::mat4(1.f);
+	Core::ResourceHandle<C_TrimeshModel> m_TrimeshModel;
+	Physics::Primitives::S_SSEAABB		 m_AABB;
+	// trimesh, maybe transformed AABB?
+};
+} // namespace GLEngine::Renderer
